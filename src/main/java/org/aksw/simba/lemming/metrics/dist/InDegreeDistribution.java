b9cb9eaeaf2b78e0efe9959ec00021b4cdaef97a
mroeder <roeder@informatik.uni-leipzig.de>
2016-03-21 10:55:37 +0100

    Moved test cases. Implemented the InDegree distribution.

---
+++ src/main/java/org/aksw/simba/lemming/metrics/dist/InDegreeDistribution.java
@@ -0,0 +1,42 @@
+package org.aksw.simba.lemming.metrics.dist;
+
+import java.util.Arrays;
+
+import org.aksw.simba.lemming.ColouredGraph;
+
+import com.carrotsearch.hppc.IntArrayList;
+import com.carrotsearch.hppc.IntIntOpenHashMap;
+
+import grph.Grph;
+
+/**
+ * The distribution of in degrees. The sample space is sorted ascending.
+ * 
+ * @author Michael R&ouml;der (roeder@informatik.uni-leipzig.de)
+ *
+ */
+public class InDegreeDistribution extends AbstractDistributionalMetric {
+
+    public InDegreeDistribution() {
+        super("inDegreeDist");
+    }
+
+    @Override
+    public void apply(ColouredGraph graph) {
+        IntIntOpenHashMap counts = new IntIntOpenHashMap();
+        Grph g = graph.getGraph();
+        IntArrayList inDegrees = g.getAllInEdgeDegrees();
+        for (int i = 0; i < inDegrees.elementsCount; ++i) {
+            counts.putOrAdd(inDegrees.buffer[i], 1, 1);
+        }
+
+        sampleSpace = new Object[counts.assigned];
+        distribution = new double[counts.assigned];
+        int keys[] = counts.keys().toArray();
+        Arrays.sort(keys);
+        for (int j = 0; j < keys.length; ++j) {
+            sampleSpace[j] = counts.get(keys[j]);
+            sampleSpace[j] = keys[j];
+        }
+    }
+}
