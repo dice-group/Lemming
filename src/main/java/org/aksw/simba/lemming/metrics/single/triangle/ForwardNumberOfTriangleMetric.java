860bd916c95a6ac3457db34de0a1359d87dd6809
Alexander Hetzer <alexander.hetzer@gmail.com>
2018-01-06 16:40:29 +0100

    Add ForwardNumberOfTriangleMetric as triangle counting metric

---
+++ src/main/java/org/aksw/simba/lemming/metrics/single/triangle/ForwardNumberOfTriangleMetric.java
@@ -0,0 +1,78 @@
+package org.aksw.simba.lemming.metrics.single.triangle;
+
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.aksw.simba.lemming.ColouredGraph;
+import org.aksw.simba.lemming.metrics.AbstractMetric;
+import org.aksw.simba.lemming.metrics.single.SingleValueMetric;
+
+import com.carrotsearch.hppc.cursors.IntCursor;
+import com.google.common.collect.Sets;
+
+import toools.set.IntSet;
+
+
+public class ForwardNumberOfTriangleMetric extends AbstractMetric implements SingleValueMetric {
+
+   private List<HashSet<Integer>> adjacencyDatastructure;
+   private ColouredGraph coloredGraph;
+
+   private int amountOfTriangles;
+
+
+   public ForwardNumberOfTriangleMetric() {
+      super("forward #triangles");
+   }
+
+
+   @Override
+   public double apply(ColouredGraph coloredGraph) {
+      initialize(coloredGraph);
+      for (Integer nodeId : getOrderedNodes()) {
+         // process nodes reached by outgoing edges
+         processNeighbors(nodeId, coloredGraph.getOutNeighbors(nodeId));
+         // process nodes reached by ingoing edges
+         processNeighbors(nodeId, coloredGraph.getInNeighbors(nodeId));
+      }
+      return amountOfTriangles;
+   }
+
+
+   private void processNeighbors(int nodeId, IntSet neighborSet) {
+      for (IntCursor adjacentNodeIdCursor : neighborSet) {
+         int adjacentNodeId = adjacentNodeIdCursor.value;
+         if (nodeId < adjacentNodeId) {
+            amountOfTriangles += Sets.intersection(adjacencyDatastructure.get(nodeId), adjacencyDatastructure.get(adjacentNodeId)).size();
+            adjacencyDatastructure.get(adjacentNodeId).add(nodeId);
+         }
+      }
+   }
+
+
+   private void initialize(ColouredGraph coloredGraph) {
+      amountOfTriangles = 0;
+      this.coloredGraph = coloredGraph;
+      adjacencyDatastructure = new ArrayList<>(coloredGraph.getVertices().size());
+      for (int i = 0; i < coloredGraph.getVertices().size(); i++) {
+         adjacencyDatastructure.add(new HashSet<Integer>());
+      }
+   }
+
+
+   private List<Integer> getOrderedNodes() {
+      List<Integer> orderedNodes = coloredGraph.getVertices().toIntegerArrayList().stream()
+            .sorted((node1, node2) -> Integer.compare(getTotalAmountOfNeighbors(node2), getTotalAmountOfNeighbors(node1)))
+            .collect(Collectors.toList());
+      return orderedNodes;
+   }
+
+
+   private int getTotalAmountOfNeighbors(int nodeId) {
+      return coloredGraph.getInNeighbors(nodeId).size() + coloredGraph.getOutNeighbors(nodeId).size();
+   }
+
+}
