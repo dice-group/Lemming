3e4fe21be84469ea9fd96f11531bd8e52625ce8e
Tanja Tornede <tanja.tornede@gmail.com>
2018-01-06 16:32:36 +0100

    Refactored EdgeIteratorNumberOfTrianglesMetricTest.
    Renamed this test class to a generic number of triangle metric test
    class, so that all those metrics can be added to a list of metrics. For
    each metric of this list the counted triangles will be checked for the
    same graph.

---
+++ src/test/java/org/aksw/simba/lemming/metrics/single/triangle/NumberOfTrianglesMetricTest.java
@@ -0,0 +1,45 @@
+package org.aksw.simba.lemming.metrics.single.triangle;
+
+
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.aksw.simba.lemming.ColouredGraph;
+import org.aksw.simba.lemming.creation.GraphCreator;
+import org.aksw.simba.lemming.metrics.single.SingleValueMetric;
+import org.apache.commons.io.IOUtils;
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.ModelFactory;
+import org.junit.Assert;
+import org.junit.Test;
+
+
+public class NumberOfTrianglesMetricTest {
+
+   private static final int EXPECTED_TRIANGLES = 1;
+   private static final String GRAPH_FILE = "graph1.n3";
+
+
+   @Test
+   public void test() {
+      List<SingleValueMetric> metrics = new ArrayList<>();
+      metrics.add(new EdgeIteratorNumberOfTrianglesMetric());
+      metrics.add(new NodeIteratorCoreNumberOfTrianglesMetric());
+
+      Model model = ModelFactory.createDefaultModel();
+      InputStream is = this.getClass().getClassLoader().getResourceAsStream(GRAPH_FILE);
+      model.read(is, null, "N3");
+      IOUtils.closeQuietly(is);
+
+      GraphCreator creator = new GraphCreator();
+      ColouredGraph graph = creator.processModel(model);
+      Assert.assertNotNull(graph);
+
+      for (SingleValueMetric metric : metrics) {
+         double countedTriangles = metric.apply(graph);
+         Assert.assertEquals(EXPECTED_TRIANGLES, countedTriangles, 0.000001);
+      }
+
+   }
+}
