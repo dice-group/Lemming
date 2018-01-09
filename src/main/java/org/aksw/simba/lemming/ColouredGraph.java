22e2ad82aa6d6325893485848cd9ae93b5a81dd3
Alexander Hetzer <alexander.hetzer@gmail.com>
2018-01-09 19:04:21 +0100

    Add copy method to ColouredGraph

--- src/main/java/org/aksw/simba/lemming/ColouredGraph.java
+++ src/main/java/org/aksw/simba/lemming/ColouredGraph.java
@@ -1,145 +1,176 @@
 package org.aksw.simba.lemming;
 
-import grph.Grph;
-import grph.GrphAlgorithmCache;
-import grph.in_memory.InMemoryGrph;
 
 import org.aksw.simba.lemming.colour.ColourPalette;
 import org.aksw.simba.lemming.grph.DiameterAlgorithm;
 
-import toools.set.IntSet;
-
 import com.carrotsearch.hppc.BitSet;
 import com.carrotsearch.hppc.ObjectArrayList;
 
+import grph.Grph;
+import grph.GrphAlgorithmCache;
+import grph.in_memory.InMemoryGrph;
+import toools.set.IntSet;
+
+
 public class ColouredGraph {
 
-	protected Grph graph;
-	protected ObjectArrayList<BitSet> vertexColours = new ObjectArrayList<BitSet>();
-	protected ObjectArrayList<BitSet> edgeColours = new ObjectArrayList<BitSet>();
-	protected ColourPalette vertexPalette;
-	protected ColourPalette edgePalette;
+   protected Grph graph;
+   protected ObjectArrayList<BitSet> vertexColours = new ObjectArrayList<>();
+   protected ObjectArrayList<BitSet> edgeColours = new ObjectArrayList<>();
+   protected ColourPalette vertexPalette;
+   protected ColourPalette edgePalette;
 
-	protected GrphAlgorithmCache<Integer> diameterAlgorithm;
+   protected GrphAlgorithmCache<Integer> diameterAlgorithm;
 
-	public ColouredGraph() {
-		this(null, null);
-	}
 
-	public ColouredGraph(ColourPalette vertexPalette, ColourPalette edgePalette) {
-		this(new InMemoryGrph(), vertexPalette, edgePalette);
-	}
+   public ColouredGraph() {
+      this(null, null);
+   }
 
-	public ColouredGraph(Grph graph, ColourPalette vertexPalette, ColourPalette edgePalette) {
-		setGraph(graph);
-		this.vertexPalette = vertexPalette;
-		this.edgePalette = edgePalette;
 
-	}
+   public ColouredGraph(ColourPalette vertexPalette, ColourPalette edgePalette) {
+      this(new InMemoryGrph(), vertexPalette, edgePalette);
+   }
 
-	public Grph getGraph() {
-		return graph;
-	}
 
-	protected void setGraph(Grph graph) {
-		this.graph = graph;
-		diameterAlgorithm = new DiameterAlgorithm().cacheResultForGraph(graph);
-	}
+   public ColouredGraph(Grph graph, ColourPalette vertexPalette, ColourPalette edgePalette) {
+      setGraph(graph);
+      this.vertexPalette = vertexPalette;
+      this.edgePalette = edgePalette;
 
-	public ObjectArrayList<BitSet> getVertexColours() {
-		return vertexColours;
-	}
+   }
 
-	public ObjectArrayList<BitSet> getEdgeColours() {
-		return edgeColours;
-	}
 
-	public int addVertex() {
-		return addVertex(new BitSet());
-	}
+   public Grph getGraph() {
+      return graph;
+   }
 
-	public int addVertex(BitSet colour) {
-		int id = graph.addVertex();
-		vertexColours.add(colour);
-		return id;
-	}
 
-	public int addEdge(int tail, int head) {
-		return addEdge(tail, head, new BitSet());
-	}
+   protected void setGraph(Grph graph) {
+      this.graph = graph;
+      diameterAlgorithm = new DiameterAlgorithm().cacheResultForGraph(graph);
+   }
 
-	public int addEdge(int tail, int head, BitSet colour) {
-		int id = graph.addDirectedSimpleEdge(tail, head);
-		edgeColours.add(new BitSet());
-		return id;
-	}
 
-	public void setVertexColour(int vertexId, BitSet colour) {
-		if (vertexId < vertexColours.elementsCount) {
-			((Object[]) vertexColours.buffer)[vertexId] = colour;
-		}
-	}
+   public ObjectArrayList<BitSet> getVertexColours() {
+      return vertexColours;
+   }
 
-	public void setEdgeColour(int edgeId, BitSet colour) {
-		if (edgeId < edgeColours.elementsCount) {
-			((Object[]) edgeColours.buffer)[edgeId] = colour;
-		}
-	}
 
-	public BitSet getVertexColour(int vertexId) {
-		if (vertexId < vertexColours.elementsCount) {
-			return (BitSet) ((Object[]) vertexColours.buffer)[vertexId];
-		} else {
-			return new BitSet();
-		}
-	}
+   public ObjectArrayList<BitSet> getEdgeColours() {
+      return edgeColours;
+   }
 
-	public BitSet getEdgeColour(int edgeId) {
-		if (edgeId < vertexColours.elementsCount) {
-			return (BitSet) ((Object[]) vertexColours.buffer)[edgeId];
-		} else {
-			return new BitSet();
-		}
-	}
 
-	public ColourPalette getVertexPalette() {
-		return vertexPalette;
-	}
+   public int addVertex() {
+      return addVertex(new BitSet());
+   }
 
-	public ColourPalette getEdgePalette() {
-		return edgePalette;
-	}
 
-	public int[][] getInNeighborhoodsArray() {
-		return graph.getInNeighborhoods();
-	}
+   public int addVertex(BitSet colour) {
+      int id = graph.addVertex();
+      vertexColours.add(colour);
+      return id;
+   }
 
-	public int[][] getOutNeighborhoodsArray() {
-		return graph.getOutNeighborhoods();
-	}
 
-	public IntSet getInNeighbors(int v) {
-		return graph.getInNeighbors(v);
-	}
+   public int addEdge(int tail, int head) {
+      return addEdge(tail, head, new BitSet());
+   }
 
-	public IntSet getOutNeighbors(int v) {
-		return graph.getOutNeighbors(v);
-	}
 
-	public IntSet getVertices() {
-		return graph.getVertices();
-	}
+   public int addEdge(int tail, int head, BitSet colour) {
+      int id = graph.addDirectedSimpleEdge(tail, head);
+      edgeColours.add(new BitSet());
+      return id;
+   }
 
-	public IntSet getOutEdges(int vertexId) {
-		return graph.getOutEdges(vertexId);
-	}
 
-	public IntSet getVerticesAccessibleThrough(int vertexId, int edgeId) {
-		return graph.getVerticesAccessibleThrough(vertexId, edgeId);
-	}
+   public void setVertexColour(int vertexId, BitSet colour) {
+      if (vertexId < vertexColours.elementsCount) {
+         vertexColours.buffer[vertexId] = colour;
+      }
+   }
 
-	public int getDiameter() {
-		return diameterAlgorithm.compute(graph);
-	}
+
+   public void setEdgeColour(int edgeId, BitSet colour) {
+      if (edgeId < edgeColours.elementsCount) {
+         edgeColours.buffer[edgeId] = colour;
+      }
+   }
+
+
+   public BitSet getVertexColour(int vertexId) {
+      if (vertexId < vertexColours.elementsCount) {
+         return (BitSet) vertexColours.buffer[vertexId];
+      } else {
+         return new BitSet();
+      }
+   }
+
+
+   public BitSet getEdgeColour(int edgeId) {
+      if (edgeId < vertexColours.elementsCount) {
+         return (BitSet) vertexColours.buffer[edgeId];
+      } else {
+         return new BitSet();
+      }
+   }
+
+
+   public ColourPalette getVertexPalette() {
+      return vertexPalette;
+   }
+
+
+   public ColourPalette getEdgePalette() {
+      return edgePalette;
+   }
+
+
+   public int[][] getInNeighborhoodsArray() {
+      return graph.getInNeighborhoods();
+   }
+
+
+   public int[][] getOutNeighborhoodsArray() {
+      return graph.getOutNeighborhoods();
+   }
+
+
+   public IntSet getInNeighbors(int v) {
+      return graph.getInNeighbors(v);
+   }
+
+
+   public IntSet getOutNeighbors(int v) {
+      return graph.getOutNeighbors(v);
+   }
+
+
+   public IntSet getVertices() {
+      return graph.getVertices();
+   }
+
+
+   public IntSet getOutEdges(int vertexId) {
+      return graph.getOutEdges(vertexId);
+   }
+
+
+   public IntSet getVerticesAccessibleThrough(int vertexId, int edgeId) {
+      return graph.getVerticesAccessibleThrough(vertexId, edgeId);
+   }
+
+
+   public int getDiameter() {
+      return diameterAlgorithm.compute(graph);
+   }
+
+
+   public ColouredGraph copy() {
+      return new ColouredGraph(graph, vertexPalette, edgePalette);
+   }
 
 }
