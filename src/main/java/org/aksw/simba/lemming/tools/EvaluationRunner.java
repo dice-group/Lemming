3f5f4b60ddb4be23648b625953ca697007265222
Alexander Hetzer <alexander.hetzer@gmail.com>
2018-01-11 16:16:37 +0100

    Adapt evaluation classes wrt naming and reference graphs
    Adapted the evaluation classes in such a way that they make use of the
    graph names in their debug output. Furthermore added the evaluation on
    reference graphs (such as cliques, rings, etc.) based on the original
    graphs read in.

--- src/main/java/org/aksw/simba/lemming/tools/EvaluationRunner.java
+++ src/main/java/org/aksw/simba/lemming/tools/EvaluationRunner.java
@@ -11,11 +11,17 @@ import org.aksw.simba.lemming.ColouredGraph;
 import org.aksw.simba.lemming.creation.SemanticWebDogFoodReader;
 import org.aksw.simba.lemming.creation.SimpleGraphFormatReader;
 import org.aksw.simba.lemming.metrics.single.SingleValueMetric;
-import org.aksw.simba.lemming.metrics.single.triangle.EdgeIteratorNumberOfTrianglesMetric;
 import org.aksw.simba.lemming.metrics.single.triangle.NodeIteratorCoreNumberOfTrianglesMetric;
+import org.aksw.simba.lemming.metrics.single.triangle.NodeIteratorNumberOfTrianglesMetric;
 import org.aksw.simba.lemming.metrics.single.triangle.forward.ForwardNumberOfTriangleMetric;
+import org.aksw.simba.lemming.metrics.single.triangle.matrix.MatrixMultiplicationNumberOfTrianglesMetric;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import grph.Grph;
+import grph.algo.topology.ClassicalGraphs;
+import grph.algo.topology.StarTopologyGenerator;
+import grph.in_memory.InMemoryGrph;
 
 
 public class EvaluationRunner {
@@ -28,15 +34,25 @@ public class EvaluationRunner {
 
    public static void main(String args[]) {
 
-      List<SingleValueMetric> metrics = Arrays.asList(new ForwardNumberOfTriangleMetric(), new NodeIteratorCoreNumberOfTrianglesMetric(),
-            new EdgeIteratorNumberOfTrianglesMetric());
+      List<SingleValueMetric> metrics = Arrays.asList(new ForwardNumberOfTriangleMetric(), new NodeIteratorNumberOfTrianglesMetric(),
+            new NodeIteratorCoreNumberOfTrianglesMetric(), new MatrixMultiplicationNumberOfTrianglesMetric());
 
       List<ColouredGraph> graphs = new ArrayList<>();
-      graphs.addAll(getSnapEvaluationGraphs());
+      // graphs.addAll(getSnapEvaluationGraphs());
       graphs.addAll(getSemanticDogFoodGraphs());
 
-      NumberOfTriangleMetricsPerformanceEvaluation evaluation = new NumberOfTriangleMetricsPerformanceEvaluation(metrics, graphs);
-      evaluation.evaluatePerformance();
+      NumberOfTriangleMetricsPerformanceEvaluation evaluation = new NumberOfTriangleMetricsPerformanceEvaluation(metrics);
+      System.out.println(graphs.size());
+      for (ColouredGraph graph : graphs) {
+         LOGGER.info("===STARTING NEXT MAIN GRAPH===");
+         evaluation.evaluatePerformance(graph);
+         evaluation.evaluatePerformance(generateReferenceStarGraph(graph));
+         evaluation.evaluatePerformance(generateReferenceGridGraph(graph));
+         evaluation.evaluatePerformance(generateReferenceRingGraph(graph));
+         evaluation.evaluatePerformance(generateReferenceCliqueGraph(graph));
+         evaluation.evaluatePerformance(generateReferenceCompleteBipartiteGraph(graph));
+      }
+
    }
 
 
@@ -60,6 +76,39 @@ public class EvaluationRunner {
          }
       }
       return graphs;
+   }
+
+
+   private static ColouredGraph generateReferenceStarGraph(ColouredGraph graph) {
+      StarTopologyGenerator starGenerator = new StarTopologyGenerator();
+      Grph starGraph = new InMemoryGrph();
+      starGraph.addNVertices(graph.getGraph().getNumberOfVertices());
+      starGenerator.compute(starGraph);
+      return new ColouredGraph(starGraph, graph.toString() + "-star");
+   }
+
+
+   private static ColouredGraph generateReferenceGridGraph(ColouredGraph graph) {
+      int partSize = (int) Math.sqrt(graph.getGraph().getNumberOfVertices());
+      return new ColouredGraph(ClassicalGraphs.grid(partSize, partSize), graph.toString() + "-grid");
+   }
+
+
+   private static ColouredGraph generateReferenceRingGraph(ColouredGraph graph) {
+      int numberOfNodes = graph.getGraph().getNumberOfVertices();
+      return new ColouredGraph(ClassicalGraphs.cycle(numberOfNodes), graph.toString() + "-ring");
+   }
+
+
+   private static ColouredGraph generateReferenceCliqueGraph(ColouredGraph graph) {
+      int partSize = (int) Math.sqrt(graph.getGraph().getNumberOfVertices());
+      return new ColouredGraph(ClassicalGraphs.completeGraph(partSize), graph.toString() + "-clique");
+   }
+
+
+   private static ColouredGraph generateReferenceCompleteBipartiteGraph(ColouredGraph graph) {
+      int partSize = (int) (graph.getGraph().getNumberOfVertices() / 8.0);
+      return new ColouredGraph(ClassicalGraphs.completeBipartiteGraph(partSize, partSize), graph.toString() + "-bipartite graph");
    }
 
 
--- src/main/java/org/aksw/simba/lemming/tools/NumberOfTriangleMetricsPerformanceEvaluation.java
+++ src/main/java/org/aksw/simba/lemming/tools/NumberOfTriangleMetricsPerformanceEvaluation.java
@@ -1,6 +1,7 @@
 package org.aksw.simba.lemming.tools;
 
 
+import java.util.Collections;
 import java.util.List;
 
 import org.aksw.simba.lemming.ColouredGraph;
@@ -18,6 +19,11 @@ public class NumberOfTriangleMetricsPerformanceEvaluation {
    private List<ColouredGraph> graphs;
 
 
+   public NumberOfTriangleMetricsPerformanceEvaluation(List<SingleValueMetric> singleValueMetrics) {
+      this(singleValueMetrics, Collections.emptyList());
+   }
+
+
    public NumberOfTriangleMetricsPerformanceEvaluation(List<SingleValueMetric> singleValueMetrics, List<ColouredGraph> graphs) {
       super();
       this.singleValueMetrics = singleValueMetrics;
@@ -27,7 +33,8 @@ public class NumberOfTriangleMetricsPerformanceEvaluation {
 
    public void evaluatePerformance() {
       for (ColouredGraph graph : graphs) {
-         LOGGER.info("Starting computation of graph with size {}.", graph.getVertices().size());
+         LOGGER.info("Evaluating graph {} with {} nodes and {} edges.", graph, graph.getGraph().getNumberOfVertices(),
+               graph.getGraph().getNumberOfEdges());
          for (SingleValueMetric metric : singleValueMetrics) {
             long startTimeInMilliseconds = System.currentTimeMillis();
             double amountOfTriangles = metric.apply(graph);
@@ -37,5 +44,16 @@ public class NumberOfTriangleMetricsPerformanceEvaluation {
       }
    }
 
+
+   public void evaluatePerformance(ColouredGraph graph) {
+      LOGGER.info("Evaluating graph {} with {} nodes and {} edges.", graph, graph.getGraph().getNumberOfVertices(),
+            graph.getGraph().getNumberOfEdges());
+      for (SingleValueMetric metric : singleValueMetrics) {
+         long startTimeInMilliseconds = System.currentTimeMillis();
+         double amountOfTriangles = metric.apply(graph);
+         long requiredTimeInMilliseconds = System.currentTimeMillis() - startTimeInMilliseconds;
+         LOGGER.info(String.format("%-30s%10d %10f", metric, (int) amountOfTriangles, requiredTimeInMilliseconds / 1000.0));
+      }
+   }
 
 }
