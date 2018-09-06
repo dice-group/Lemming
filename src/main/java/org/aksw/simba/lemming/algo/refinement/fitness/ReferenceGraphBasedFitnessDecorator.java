f37146e6bf39ef7989514c10a483e9bb179f68f6
Micha <michael.roeder@uni-paderborn.de>
2018-09-06 15:12:29 +0200

    Changed the ReferenceGraphBasedFitness to user 1 - reference fitness instead of fitness / reference fitness. Added an abstract implementation for the fitness decorators.

--- src/main/java/org/aksw/simba/lemming/algo/refinement/fitness/ReferenceGraphBasedFitnessDecorator.java
+++ src/main/java/org/aksw/simba/lemming/algo/refinement/fitness/ReferenceGraphBasedFitnessDecorator.java
@@ -4,28 +4,33 @@ import org.aksw.simba.lemming.algo.expression.Expression;
 
 import com.carrotsearch.hppc.ObjectDoubleOpenHashMap;
 
-public class ReferenceGraphBasedFitnessDecorator implements FitnessFunctionDecorator {
+public class ReferenceGraphBasedFitnessDecorator extends AbstractFitnessFunctionDecorator {
 
-    protected FitnessFunction decorated;
     protected ObjectDoubleOpenHashMap<String>[] referenceGraphVectors;
 
     public ReferenceGraphBasedFitnessDecorator(FitnessFunction decorated,
             ObjectDoubleOpenHashMap<String>[] referenceGraphVectors) {
-        this.decorated = decorated;
+        super(decorated);
         this.referenceGraphVectors = referenceGraphVectors;
     }
 
     @Override
     public double getFitness(Expression expression, ObjectDoubleOpenHashMap<String>[] graphVectors) {
         double fitness = decorated.getFitness(expression, graphVectors);
-        double comparedFitness = fitness / decorated.getFitness(expression, referenceGraphVectors);
+        double comparedFitness = 1 - decorated.getFitness(expression, referenceGraphVectors);
+//        double comparedFitness = fitness / decorated.getFitness(expression, referenceGraphVectors);
+        // Messages needed for debugging
+//        System.out.print(expression.toString());
+//        System.out.print(" --> f=");
+//        System.out.print(fitness);
+//        System.out.print(" r=");
+//        System.out.print(decorated.getFitness(expression, referenceGraphVectors));
+//        System.out.print(" c=");
+//        System.out.print(comparedFitness);
+//        System.out.print(" v=");
+//        System.out.println((2 * fitness * comparedFitness) / (fitness + comparedFitness));
         return (2 * fitness * comparedFitness) / (fitness + comparedFitness);
         // return comparedFitness;
-    }
-
-    @Override
-    public FitnessFunction getDecorated() {
-        return decorated;
     }
 
 }
