ff8b75800143ce8ceb733d4d667beac25e2a7abb
Pham Thuy Sy Nguyen <p.nguyen@woelke.net>
2018-09-12 04:46:42 +0200

    test number of coloured vertices and edges
    -remove system.exit when done

--- src/main/java/org/aksw/simba/lemming/mimicgraph/generator/AbstractGraphGeneration.java
+++ src/main/java/org/aksw/simba/lemming/mimicgraph/generator/AbstractGraphGeneration.java
@@ -494,7 +494,7 @@ public abstract class AbstractGraphGeneration {
 		
 		ExecutorService service = Executors.newFixedThreadPool(mNumberOfThreads);
 		
-		LOGGER.info("Create "+lstAssignedEdges.size()+" threads for painting edges !");
+		LOGGER.info("Create "+lstAssignedEdges.size()+" threads for painting edges....!");
 		
 		List<Callable<Object>> tasks = new ArrayList<Callable<Object>>();
 		final Set<BitSet> setOfRestrictedEdgeColours = new HashSet<BitSet>(mSetOfRestrictedEdgeColours);
@@ -528,8 +528,8 @@ public abstract class AbstractGraphGeneration {
 					//set of edges for painting
 					int[] arrOfEdges = setOfEdges.toIntArray();
 					
-					LOGGER.info("Thread " + indexOfThread +" is painting " + arrOfEdges.length +" edges with "
-											+ setOfRestrictedEdgeColours.size()+" colours... ");
+					//LOGGER.info("Thread " + indexOfThread +" is painting " + arrOfEdges.length +" edges with "
+					//						+ setOfRestrictedEdgeColours.size()+" colours... ");
 					int j = 0 ; 
 					while(j < arrOfEdges.length){
 						BitSet offeredColor = (BitSet) eColoProposer.getPotentialItem(setOfRestrictedEdgeColours, true);
