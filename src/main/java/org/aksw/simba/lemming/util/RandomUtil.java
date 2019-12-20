5b34ea167240087269e292ec29200b92f29e9b1b
Micha <michael.roeder@uni-paderborn.de>
2019-12-20 18:22:53 +0100

    Fixed an issue in the RandomUtil class.

--- src/main/java/org/aksw/simba/lemming/util/RandomUtil.java
+++ src/main/java/org/aksw/simba/lemming/util/RandomUtil.java
@@ -1,23 +1,23 @@
 package org.aksw.simba.lemming.util;
 
+import java.util.OptionalInt;
 import java.util.Random;
 import java.util.Set;
 
 public class RandomUtil {
-	
-	public static int getRandomWithExclusion(Random rnd, int bound, Set<Integer> exclude) {
-	    int random = rnd.nextInt(bound);
-	    for (int ex : exclude) {
-	        if (random == ex && random +1 < bound) {
-	        	random++;
-	        } else {
-	        	return random;
-	        }
-	        
-	    }
-	    return random;
-	}
 
-	
+    public static int getRandomWithExclusion(Random rnd, int bound, Set<Integer> exclude) {
+        if (exclude.size() >= bound) {
+            throw new IllegalArgumentException(
+                    "Can't choose a random number since all possible numbers seem to be excluded.");
+        }
+        OptionalInt result = rnd.ints(bound).filter(i -> !exclude.contains(i)).findFirst();
+        if (result.isPresent()) {
+            return result.getAsInt();
+        } else {
+            throw new IllegalStateException(
+                    "The random number generation ended without a result. This shouldn't happen.");
+        }
+    }
 
 }
