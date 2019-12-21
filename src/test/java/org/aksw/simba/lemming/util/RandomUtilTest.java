121ffe28a09893c4d7a2448eb4e68bdff7188459
Micha <michael.roeder@uni-paderborn.de>
2019-12-21 15:59:17 +0100

    Extended the test for the RandumUtil class regarding illegal arguments.

--- src/test/java/org/aksw/simba/lemming/util/RandomUtilTest.java
+++ src/test/java/org/aksw/simba/lemming/util/RandomUtilTest.java
@@ -5,6 +5,8 @@ import java.util.Collection;
 import java.util.List;
 import java.util.Random;
 import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import org.junit.Assert;
 import org.junit.Test;
@@ -17,35 +19,43 @@ import com.google.common.collect.Sets;
 @RunWith(Parameterized.class)
 public class RandomUtilTest {
 
-	@Parameters
-	public static Collection<Object[]> data() {
-		List<Object[]> testConfigs = new ArrayList<Object[]>();
+    @Parameters
+    public static Collection<Object[]> data() {
+        List<Object[]> testConfigs = new ArrayList<Object[]>();
 
-		testConfigs.add(new Object[] { Sets.newHashSet(1, 2), 5 });
-		testConfigs.add(new Object[] { Sets.newHashSet(13423, 212), 250 });
-		testConfigs.add(new Object[] { Sets.newHashSet(11, 2, 67, 998), 2 });
+        testConfigs.add(new Object[] { Sets.newHashSet(1, 2), 5 });
+        testConfigs.add(new Object[] { Sets.newHashSet(13423, 212), 250 });
+        testConfigs.add(new Object[] { Sets.newHashSet(11, 2, 67, 998), 2 });
 
-		return testConfigs;
-	}
+        return testConfigs;
+    }
 
-	private Set<Integer> exclusionSet;
-	private int bound;
+    private Set<Integer> exclusionSet;
+    private int bound;
 
-	public RandomUtilTest(Set<Integer> exclusionSet, int bound) {
-		this.exclusionSet = exclusionSet;
-		this.bound = bound;
-	}
+    public RandomUtilTest(Set<Integer> exclusionSet, int bound) {
+        this.exclusionSet = exclusionSet;
+        this.bound = bound;
+    }
 
-	@Test
-	public void test() {
-		Random random = new Random();
-		int tries = 20;
-		while (tries > 0) {
-			int rand = RandomUtil.getRandomWithExclusion(random, bound, exclusionSet);
-			tries--;
+    @Test
+    public void test() {
+        Random random = new Random();
+        int tries = 20;
+        while (tries > 0) {
+            int rand = RandomUtil.getRandomWithExclusion(random, bound, exclusionSet);
+            tries--;
 
-			Assert.assertFalse(exclusionSet.contains(rand));
-			Assert.assertFalse(rand > bound);
-		}
-	}
+            Assert.assertFalse(exclusionSet.contains(rand));
+            Assert.assertFalse(rand > bound);
+        }
+    }
+
+    @Test(expected = IllegalArgumentException.class,timeout=2000)
+    public void testFailing() {
+        Random random = new Random();
+        Set<Integer> excludeAllSet = IntStream.range(0, bound).mapToObj(i -> new Integer(i))
+                .collect(Collectors.toSet());
+        RandomUtil.getRandomWithExclusion(random, bound, excludeAllSet);
+    }
 }
