6099cf909e9125df2d13ef24a46632a00a673635
wangzun66 <wangzun666@gmail.com>
2021-05-07 23:17:56 +0200

    add javadoc for ListingAyzMetric

--- src/main/java/org/aksw/simba/lemming/metrics/single/nodetriangles/ayz/ListingAyzMetric.java
+++ src/main/java/org/aksw/simba/lemming/metrics/single/nodetriangles/ayz/ListingAyzMetric.java
@@ -18,27 +18,38 @@ import org.aksw.simba.lemming.util.Constants;
  * "Finding and Counting Given Length Cycles".
  *
  * @see <a href=
- *      "https://www.researchgate.net/publication/225621879_Finding_and_Counting_Given_Length_Cycles">https://www.researchgate.net/publication/225621879_Finding_and_Counting_Given_Length_Cycles</a>).
+ *      "https://www.researchgate.net/publication/225621879_Finding_and_Counting_Given_Length_Cycles">https://www.researchgate.net/publication/225621879_Finding_and_Counting_Given_Length_Cycles</a>
  *
- * @see <a>https://i11www.iti.kit.edu/extra/publications/sw-fclt-05_t.pdf</a>
+ * @see <a href=
+ *      "https://i11www.iti.kit.edu/extra/publications/sw-fclt-05_t.pdf">https://i11www.iti.kit.edu/extra/publications/sw-fclt-05_t.pdf</a>
  *
- * @author Tanja Tornede
+ * @author Tanja Tornede, Zun Wang
  *
  */
 public class ListingAyzMetric extends AbstractMetric implements SingleValueMetric {
 
+    /**
+     * &gamma; is so-called matrix multiplication exponent. According to the publication "Finding, Counting and Listing
+     * all Triangles in Large Graph", if use an adjacency matrix for triangle counting algorithm, then the runtime of
+     * the algorithm is &Theta;(n<sup>&gamma;</sup>) with &gamma; &le; 2.376.
+     */
+    private static final double GAMMA = 2.376;
+    /**
+     * This is used to split the node set into low degree vertices V<sub>low</sub>={v &isin; V : d(v) &le; &beta;} and
+     * high degree vertices V<sub>high</sub> = V \ V<sub>low</sub>, where &beta;=m<sup>EXPONENT</sup> and m is number of
+     * edges.
+     */
+    private static final double EXPONENT = (GAMMA-1)/(GAMMA+1);
 
     public ListingAyzMetric() {
         super("#nodetriangles");
     }
 
-
     @Override
     public double apply(ColouredGraph graph) {
 
-        double r = 2.376;
-        double threshold = Math.pow(graph.getGraph().getNumberOfEdges(), (r - 1) / (r + 1));
-        IntSet highDegreeVertices = new DefaultIntSet(Constants.DEFAULT_SIZE);
+        double threshold = Math.pow(graph.getGraph().getNumberOfEdges(), EXPONENT);
+        IntSet highDegreeVertices = new DefaultIntSet(graph.getVertices().size());
 
         for (int vertex:graph.getVertices()) {
             if (graph.getGraph().getVertexDegree(vertex) > threshold) {
@@ -55,13 +66,25 @@ public class ListingAyzMetric extends AbstractMetric implements SingleValueMetri
         return numberOfTriangles;
     }
 
-
+    /**
+     * Perform the {@link NodeIteratorMetric} to count the number of triangles on the nodes set V \ V<sub>subset</sub>
+     * from a given {@link ColouredGraph}.
+     * @param graph an instance of {@link ColouredGraph}.
+     * @param vertices subset V<sub>subset</sub> &sube; V, where V is nodes set of the given graph.
+     * @return Number of triangles on the nodes set V\vertices.
+     */
     private double countTrianglesViaNodeIterator(ColouredGraph graph, IntSet vertices) {
         NodeIteratorMetric nodeIterator = new NodeIteratorMetric();
         return nodeIterator.calculateTriangles(graph, vertices);
     }
 
-
+    /**
+     * Perform the {@link MatrixMultiplicationNumberOfTrianglesMetric} to count the number of triangles on the induced
+     * subgraph of nodes subset V<sub>subset</sub> from a given {@link ColouredGraph}.
+     * @param graph an instance of {@link ColouredGraph}.
+     * @param vertices subset V<sub>subset</sub> &sube; V, where V is nodes set of the given graph.
+     * @return Number of triangles on the induced subgraph.
+     */
     private double countTrianglesViaMatrixMultiplication(ColouredGraph graph, IntSet vertices) {
         ColouredGraph subgraph = new ColouredGraph(graph.getGraph().getSubgraphInducedByVertices(vertices),
                 graph.getVertexPalette(), graph.getEdgePalette());
