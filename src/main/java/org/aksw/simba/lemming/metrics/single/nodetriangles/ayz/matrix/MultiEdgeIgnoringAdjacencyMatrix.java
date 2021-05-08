a75875527f40ac1c63c30c281217b078988e6516
wangzun66 <wangzun666@gmail.com>
2021-05-08 15:00:29 +0200

    improve MultiEdgeIgnoringAdjacencyMatrix and add a test

--- src/main/java/org/aksw/simba/lemming/metrics/single/nodetriangles/ayz/matrix/MultiEdgeIgnoringAdjacencyMatrix.java
+++ src/main/java/org/aksw/simba/lemming/metrics/single/nodetriangles/ayz/matrix/MultiEdgeIgnoringAdjacencyMatrix.java
@@ -8,12 +8,15 @@ import com.carrotsearch.hppc.IntIntOpenHashMap;
 import grph.Grph;
 import toools.math.IntMatrix;
 
-/*
-* Code reference:
-* https://github.com/BlackHawkLex/Lemming/blob/master/src/main/java/org/aksw/simba/lemming/metrics/single/triangle/matrix/MultiEdgeIgnoringAdjacencyMatrix.java
-*
-* */
-
+/**
+ * MultiEdgeIgnoringAdjacencyMatrix is used to build  a undirected adjacency matrix for a given directed {@link Grph}.
+ * If there's an edge (v1, v2) and v1 &ne; v2 in the graph, then enter 1 in corresponding positions (v1, v2) and (v2, v1), otherwise
+ * enter 0. So MultiEdgeIgnoringAdjacencyMatrix is a symmetric (0,1)-matrix whose entries on diagonal are 0s.
+ *
+ * Code reference:
+ * https://github.com/BlackHawkLex/Lemming/blob/master/src/main/java/org/aksw/simba/lemming/metrics/single/triangle/matrix/MultiEdgeIgnoringAdjacencyMatrix.java
+ *
+ */
 public class MultiEdgeIgnoringAdjacencyMatrix {
 
     private IntMatrix internalMatrix;
@@ -43,6 +46,12 @@ public class MultiEdgeIgnoringAdjacencyMatrix {
         }
     }
 
+    /**
+     * Calculate m<sup>k</sup>, where m is a matrix.
+     * @param adjacencyMatrix an instance of MultiEdgeIgnoringAdjacencyMatrix.
+     * @param k exponent
+     * @return a matrix = adacentcyMatrix<sup>k</sup>
+     */
     public static IntMatrix power(MultiEdgeIgnoringAdjacencyMatrix adjacencyMatrix, int k) {
         IntMatrix intMatrix = adjacencyMatrix.internalMatrix;
         IntMatrix r = new IntMatrix(intMatrix.toIntArray());
@@ -50,7 +59,6 @@ public class MultiEdgeIgnoringAdjacencyMatrix {
         for (int i = 1; i < k; i++) {
             r = IntMatrix.multiplication(r, intMatrix);
         }
-
         return r;
     }
 
