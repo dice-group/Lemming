71b95346823cf38f4760ed80160ee322a76233ff
pranav.kulkarni <pranavjk@mail.uni-paderborn.de>
2021-06-28 11:26:45 +0200

    Make Signature and Camel Case changes

--- src/main/java/org/aksw/simba/lemming/metrics/single/StdDevVertexDegree.java
+++ src/main/java/org/aksw/simba/lemming/metrics/single/StdDevVertexDegree.java
@@ -53,46 +53,23 @@ public class StdDevVertexDegree extends AvgVertexDegreeMetric {
 		SimpleMetricResult newMetricResult = (SimpleMetricResult) previousResult;
 		int[] degreesArray;
 		IntArrayList degreesList;
-		switch (getName()) {
-		case "stdDevInDegree":
-			degreesArray = mVertexDegrees.getmMapVerticesinDegree();
+		switch (direction) {
+		case in:
+			degreesArray = mVertexDegrees.getmMapVerticesInDegree();
 			degreesList = new IntArrayList(degreesArray);// Arrays.asList(ArrayUtils.toObject(degreesArray));
 			newMetricResult.setResult(calculateStdDev(degreesList, calculateAvg(degreesList)));
 			break;
 
-		case "stdDevOutDegree":
-			degreesArray = mVertexDegrees.getmMapVerticesoutDegree();
+		case out:
+			degreesArray = mVertexDegrees.getmMapVerticesOutDegree();
 			degreesList = new IntArrayList(degreesArray);
 			newMetricResult.setResult(calculateStdDev(degreesList, calculateAvg(degreesList)));
 			break;
 
 		default:// If metric is other than maxInDegree and maxOutDegree then apply the metric
-			newMetricResult = applyUpdatable(graph, previousResult);
+			newMetricResult = (SimpleMetricResult) applyUpdatable(graph);
 		}
 
 		return newMetricResult;
-	}
-
-	/**
-	 * Stores the previously computed values in UpdateMetricResult object
-	 * 
-	 * @param graph
-	 *            - input graph.
-	 * @param newMetricResult
-	 *            - UpdatableMetricResult object containing the results that should
-	 *            be updated.
-	 * @return UpdatableMetricResult object with updated sum that can be used in
-	 *         further iterations.
-	 */
-	@Override
-	public SimpleMetricResult applyUpdatable(ColouredGraph graph, UpdatableMetricResult previousResult) {
-
-		SimpleMetricResult metricResultTempObj = new SimpleMetricResult(getName(), 0.0);
-		if (previousResult instanceof SimpleMetricResult) {
-			// Copying previously computed values in temporary variables
-			metricResultTempObj.setResult(((SimpleMetricResult) previousResult).result);
-		}
-
-		return metricResultTempObj;
 	}
 }
