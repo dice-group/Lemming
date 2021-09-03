e1bcdfd5f9a70ce41f4fcfcc40b85d9d8304b95e
pranav.kulkarni <pranavjk@mail.uni-paderborn.de>
2021-09-03 19:33:46 +0200

    Add proper formatting to edited files

--- src/main/java/org/aksw/simba/lemming/metrics/single/UpdatableMetricResult.java
+++ src/main/java/org/aksw/simba/lemming/metrics/single/UpdatableMetricResult.java
@@ -9,17 +9,17 @@ package org.aksw.simba.lemming.metrics.single;
  */
 public interface UpdatableMetricResult {
 
-	/**
-	 * Returns the metric result.
-	 * 
-	 * @return - metric value.
-	 */
-	public double getResult();
+    /**
+     * Returns the metric result.
+     * 
+     * @return - metric value.
+     */
+    public double getResult();
 
-	/**
-	 * Returns the name of the metric.
-	 * 
-	 * @return - metric name.
-	 */
-	public String getMetricName();
+    /**
+     * Returns the name of the metric.
+     * 
+     * @return - metric name.
+     */
+    public String getMetricName();
 }
