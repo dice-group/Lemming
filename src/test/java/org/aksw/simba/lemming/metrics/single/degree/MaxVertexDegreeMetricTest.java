725804196deed8e2480b1be7f55c49fed90f58f1
zunhoho <wangzun666@gmail.com>
2021-10-16 00:27:47 +0200

    add test for MaxVertexDegreeMetric

--- src/test/java/org/aksw/simba/lemming/metrics/single/degree/MaxVertexDegreeMetricTest.java
+++ src/test/java/org/aksw/simba/lemming/metrics/single/degree/MaxVertexDegreeMetricTest.java
@@ -17,12 +17,18 @@ public class MaxVertexDegreeMetricTest {
         Assert.assertEquals(1.0, value);
 
         graph = removeEdge(graph, 0);
-        value = metric.update(graph, 1,-1);
+        value = metric.recompute(graph, 1,-1, false);
+        Assert.assertEquals(1.0, value);
+        value = metric.recompute(graph, 1,-1, true);
         Assert.assertEquals(1.0, value);
 
         graph = addEdge(graph, 0, 2);
-        value = metric.update(graph, 2, +1);
+        value = metric.recompute(graph, 2, +1, false);
         Assert.assertEquals(2.0, value);
+        Assert.assertEquals(1.0, metric.getCachedMaximumInDegree());
+        value = metric.recompute(graph, 2, +1, true);
+        Assert.assertEquals(2.0, value);
+        Assert.assertEquals(2.0, metric.getCachedMaximumInDegree());
 
         metric = new MaxVertexDegreeMetric(Grph.DIRECTION.out);
         graph = buildGraph1();
@@ -30,12 +36,23 @@ public class MaxVertexDegreeMetricTest {
         Assert.assertEquals(2.0, value);
 
         graph = removeEdge(graph, 0);
-        value = metric.update(graph, 0,-1);
+        value = metric.recompute(graph, 0,-1, false);
         Assert.assertEquals(1.0, value);
+        Assert.assertEquals(2.0, metric.getCachedMaximumOutDegree());
+
+        value = metric.recompute(graph, 0,-1, true);
+        Assert.assertEquals(1.0, value);
+        Assert.assertEquals(1.0, metric.getCachedMaximumOutDegree());
 
         graph = addEdge(graph, 0, 2);
-        value = metric.update(graph, 0, +1);
+        value = metric.recompute(graph, 0, +1, false);
         Assert.assertEquals(2.0, value);
+        Assert.assertEquals(1.0, metric.getCachedMaximumOutDegree());
+
+        value = metric.recompute(graph, 0, +1, true);
+        Assert.assertEquals(2.0, value);
+        Assert.assertEquals(2.0, metric.getCachedMaximumOutDegree());
+
     }
 
     @Test
@@ -46,13 +63,22 @@ public class MaxVertexDegreeMetricTest {
         Assert.assertEquals(2.0, value);
 
         graph = removeEdge(graph, 1);
-        value = metric.update(graph, 1,-1);
+        value = metric.recompute(graph, 1,-1, false);
         Assert.assertEquals(1.0, value);
+        Assert.assertEquals(2.0, metric.getCachedMaximumInDegree());
+        value = metric.recompute(graph, 1,-1, true);
+        Assert.assertEquals(1.0, value);
+        Assert.assertEquals(1.0, metric.getCachedMaximumInDegree());
 
 
         graph = addEdge(graph, 1, 1);
-        value = metric.update(graph, 1, +1);
+        value = metric.recompute(graph, 1, +1, false);
         Assert.assertEquals(2.0, value);
+        Assert.assertEquals(1.0, metric.getCachedMaximumInDegree());
+        value = metric.recompute(graph, 1, +1, true);
+        Assert.assertEquals(2.0, value);
+        Assert.assertEquals(2.0, metric.getCachedMaximumInDegree());
+
 
         metric = new MaxVertexDegreeMetric(Grph.DIRECTION.out);
         graph = buildGraph2();
@@ -60,13 +86,21 @@ public class MaxVertexDegreeMetricTest {
         Assert.assertEquals(3.0, value);
 
         graph = removeEdge(graph, 1);
-        value = metric.update(graph, 0,-1);
+        value = metric.recompute(graph, 0,-1, false);
         Assert.assertEquals(2.0, value);
-
+        Assert.assertEquals(3.0, metric.getCachedMaximumOutDegree());
+        value = metric.recompute(graph, 0,-1, true);
+        Assert.assertEquals(2.0, value);
+        Assert.assertEquals(2.0, metric.getCachedMaximumOutDegree());
 
         graph = addEdge(graph, 0, 0);
-        value = metric.update(graph, 0, +1);
+        value = metric.recompute(graph, 0, +1, false);
         Assert.assertEquals(3.0, value);
+        Assert.assertEquals(2.0, metric.getCachedMaximumOutDegree());
+        value = metric.recompute(graph, 0, +1, true);
+        Assert.assertEquals(3.0, value);
+        Assert.assertEquals(3.0, metric.getCachedMaximumOutDegree());
+
     }
 
     private ColouredGraph buildGraph1(){
