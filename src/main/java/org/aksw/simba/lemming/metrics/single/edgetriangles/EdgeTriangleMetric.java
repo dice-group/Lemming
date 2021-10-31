2d149caa87b63832a2b89d7ab7f10999a4b98b3a
zunhoho <wangzun666@gmail.com>
2021-10-31 23:56:18 +0100

    add update() in EdgeTriangleMetric

--- src/main/java/org/aksw/simba/lemming/metrics/single/edgetriangles/EdgeTriangleMetric.java
+++ src/main/java/org/aksw/simba/lemming/metrics/single/edgetriangles/EdgeTriangleMetric.java
@@ -1,9 +1,18 @@
 package org.aksw.simba.lemming.metrics.single.edgetriangles;
 
+import grph.Grph;
+import it.unimi.dsi.fastutil.ints.IntSet;
 import org.aksw.simba.lemming.ColouredGraph;
 import org.aksw.simba.lemming.metrics.AbstractMetric;
+import org.aksw.simba.lemming.metrics.MetricUtils;
 import org.aksw.simba.lemming.metrics.metricselection.EdgeTriangleMetricSelection;
+import org.aksw.simba.lemming.metrics.single.SimpleMetricResult;
 import org.aksw.simba.lemming.metrics.single.SingleValueMetric;
+import org.aksw.simba.lemming.metrics.single.UpdatableMetricResult;
+import org.aksw.simba.lemming.mimicgraph.constraints.TripleBaseSingleID;
+import org.aksw.simba.lemming.util.IntSetUtil;
+
+import javax.annotation.Nonnull;
 
 public class EdgeTriangleMetric extends AbstractMetric implements SingleValueMetric{
 
@@ -19,6 +28,53 @@ public class EdgeTriangleMetric extends AbstractMetric implements SingleValueMet
 		
 		//get number of edge triangles
 		return edgeTriangleMetric.apply(graph);
+	}
+
+	//@Override //TODO: parameters' form not determined, especially the graphOperation
+	public UpdatableMetricResult update(@Nonnull TripleBaseSingleID triple, @Nonnull ColouredGraph graph, boolean graphOperation,
+										@Nonnull UpdatableMetricResult previousResult){
+
+		Grph grph = graph.getGraph();
+		IntSet verticesConnectedToRemovingEdge = grph.getVerticesIncidentToEdge(triple.edgeId);
+
+		int headId = verticesConnectedToRemovingEdge.size() > 1 ? verticesConnectedToRemovingEdge.toIntArray()[1]
+				: verticesConnectedToRemovingEdge.toIntArray()[0];
+		int tailId = verticesConnectedToRemovingEdge.toIntArray()[0];
+
+		int numEdgesBetweenVertices = IntSetUtil.intersection(grph.getEdgesIncidentTo(tailId), grph.getEdgesIncidentTo(headId)).size();
+
+		int change = graphOperation ? -1 : 1 ;
+		int differenceOfSubGraph = calculateDifferenceOfSubGraphEdge(grph, headId, tailId, numEdgesBetweenVertices, change);
+		double newResult = previousResult.getResult() + change*differenceOfSubGraph;
+		newResult = newResult >= 0 ? newResult: 0;
+
+		return new SimpleMetricResult(previousResult.getMetricName(), newResult);
+	}
+
+	/**
+	 * It is used to calculate the difference of edge triangles in subgraph after removing or adding an edge
+	 * @param headId head of the modified edge
+	 * @param tailId tail of the modified edge
+	 * @param numEdgesBetweenVertices number of the edges between head and tail
+	 * @param change removing an edge, then -1, add an edge, then +1
+	 * @return the difference of edge triangles in subgraph after removing or adding an edge
+	 */
+	private int calculateDifferenceOfSubGraphEdge(Grph grph, int headId, int tailId, int numEdgesBetweenVertices, int change) {
+		int oldSubGraphEdgeTriangles = 0;
+		int newSubGraphTriangles = 0;
+		int newNumEdgesBetweenVertices = numEdgesBetweenVertices + change;
+
+		for (int vertex : MetricUtils.getVerticesInCommon(grph, headId, tailId)) {
+			int numEdgesFromHead = IntSetUtil.intersection(grph.getEdgesIncidentTo(headId),
+					grph.getEdgesIncidentTo(vertex)).size();
+			int numEdgesFromTail = IntSetUtil.intersection(grph.getEdgesIncidentTo(tailId),
+					grph.getEdgesIncidentTo(vertex)).size();
+			int mul = numEdgesFromHead * numEdgesFromTail;
+			oldSubGraphEdgeTriangles += (mul * numEdgesBetweenVertices);
+			newSubGraphTriangles += (mul * newNumEdgesBetweenVertices);
+		}
+
+		return change==-1 ? (oldSubGraphEdgeTriangles-newSubGraphTriangles) : (newSubGraphTriangles-oldSubGraphEdgeTriangles);
 	}
 
 }
