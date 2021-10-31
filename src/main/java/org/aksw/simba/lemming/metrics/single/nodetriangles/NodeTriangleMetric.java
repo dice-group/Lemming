b61c6be85554735530bfcab30895d60a1a7f474b
zunhoho <wangzun666@gmail.com>
2021-10-31 22:59:55 +0100

    add update() in NodeTriangleMetric

--- src/main/java/org/aksw/simba/lemming/metrics/single/nodetriangles/NodeTriangleMetric.java
+++ src/main/java/org/aksw/simba/lemming/metrics/single/nodetriangles/NodeTriangleMetric.java
@@ -1,9 +1,19 @@
 package org.aksw.simba.lemming.metrics.single.nodetriangles;
 
+import grph.Grph;
+import it.unimi.dsi.fastutil.ints.IntSet;
 import org.aksw.simba.lemming.ColouredGraph;
 import org.aksw.simba.lemming.metrics.AbstractMetric;
+import org.aksw.simba.lemming.metrics.MetricUtils;
 import org.aksw.simba.lemming.metrics.metricselection.NodeTriangleMetricSelection;
+import org.aksw.simba.lemming.metrics.single.SimpleMetricResult;
 import org.aksw.simba.lemming.metrics.single.SingleValueMetric;
+import org.aksw.simba.lemming.metrics.single.UpdatableMetricResult;
+import org.aksw.simba.lemming.metrics.single.edgemanipulation.VertexDegrees;
+import org.aksw.simba.lemming.mimicgraph.constraints.TripleBaseSingleID;
+import org.aksw.simba.lemming.util.IntSetUtil;
+
+import javax.annotation.Nonnull;
 
 public class NodeTriangleMetric extends AbstractMetric implements SingleValueMetric{
 	
@@ -21,4 +31,35 @@ public class NodeTriangleMetric extends AbstractMetric implements SingleValueMet
 		return nodeTriangleMetric.apply(graph);
 	}
 
+	//@Override //TODO: parameters' form not determined, especially the graphOperation
+	public UpdatableMetricResult update(@Nonnull TripleBaseSingleID triple, @Nonnull ColouredGraph graph, boolean graphOperation,
+										@Nonnull UpdatableMetricResult previousResult){
+
+		Grph grph = graph.getGraph();
+		IntSet verticesConnectedToRemovingEdge = grph.getVerticesIncidentToEdge(triple.edgeId);
+
+		int headId = verticesConnectedToRemovingEdge.size() > 1 ? verticesConnectedToRemovingEdge.toIntArray()[1]
+				: verticesConnectedToRemovingEdge.toIntArray()[0];
+		int tailId = verticesConnectedToRemovingEdge.toIntArray()[0];
+
+		int numEdgesBetweenVertices = IntSetUtil.intersection(grph.getEdgesIncidentTo(tailId), grph.getEdgesIncidentTo(headId)).size();
+
+		int numberOfCommon = MetricUtils.getVerticesInCommon(grph, headId, tailId).size();
+
+		//the previous result could be maintained except for 2 cases:
+		double newResult = previousResult.getResult();
+
+		//1.case: remove an edge, and number of edges between head and tail is 1
+		// -> new metric = old metric - number of common vertices
+		if(numEdgesBetweenVertices==1 && graphOperation){
+			newResult = newResult - numberOfCommon;
+
+		//2.case: add an edge, and number of edges between head and tail is 0
+		// -> new metric = old metric + number of common vertices
+		}else if (numEdgesBetweenVertices==0 && (!graphOperation)){
+			newResult = newResult + numberOfCommon;
+		}
+
+		return new SimpleMetricResult(previousResult.getMetricName(), newResult);
+	}
 }
