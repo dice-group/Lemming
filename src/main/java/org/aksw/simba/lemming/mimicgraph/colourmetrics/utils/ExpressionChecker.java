abe44dc5e1ae97a9c59be448896e061affbde75a
apundir <apundir@campus.uni-paderborn.de>
2021-11-07 23:39:45 +0100

    Created the expressions for SWDF dataset using AtomicVariable and Operation classes

--- src/main/java/org/aksw/simba/lemming/mimicgraph/colourmetrics/utils/ExpressionChecker.java
+++ src/main/java/org/aksw/simba/lemming/mimicgraph/colourmetrics/utils/ExpressionChecker.java
@@ -23,187 +23,255 @@ import com.carrotsearch.hppc.ObjectDoubleOpenHashMap;
  */
 public class ExpressionChecker {
 
-	private static final Logger LOGGER = LoggerFactory.getLogger(ExpressionChecker.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(ExpressionChecker.class);
 
-	/*
-	 * mean value of each expression key: expression
-	 */
-	private ObjectDoubleOpenHashMap<String> mMapOfMeanValues;
+    // boolean variable configured to read manual expressions
+    private boolean manualExpression = true;
+    private Set<Expression> manualExpressionsSet = new HashSet<>();
 
-	private HashMap<String, UpdatableMetricResult> mMapPrevMetricsResult; // Map to store previous metric results
+    public void setManualExpressionsSet(Set<Expression> manualExpressionsSet) {
+        this.manualExpressionsSet = manualExpressionsSet;
+    }
 
-	private ConstantValueStorage mValueCarrier;
+    /*
+     * mean value of each expression key: expression
+     */
+    private ObjectDoubleOpenHashMap<String> mMapOfMeanValues;
 
-	/*
-	 * Expressions to Increase along with difference from the mean. key: expression
-	 */
-	private ObjectDoubleOpenHashMap<Expression> mMapexpressionsToIncrease = new ObjectDoubleOpenHashMap<Expression>();
+    public void setmMapOfMeanValues(ObjectDoubleOpenHashMap<String> mMapOfMeanValues) {
+        this.mMapOfMeanValues = new ObjectDoubleOpenHashMap<>();
+        this.mMapOfMeanValues = mMapOfMeanValues;
+    }
 
-	/*
-	 * Expressions to Decrease along with difference from the mean. key: expression
-	 */
-	private ObjectDoubleOpenHashMap<Expression> mMapexpressionsToDecrease = new ObjectDoubleOpenHashMap<Expression>();
+    private HashMap<String, UpdatableMetricResult> mMapPrevMetricsResult; // Map to store previous metric results
 
-	/**
-	 * Variable to store which metric to increase.
-	 */
-	private String metricToIncrease = "";
+    private ConstantValueStorage mValueCarrier;
 
-	/**
-	 * Variable to store which metric to decrease.
-	 */
-	private String metricToDecrease = "";
+    /*
+     * Expressions to Increase along with difference from the mean. key: expression
+     */
+    private ObjectDoubleOpenHashMap<Expression> mMapexpressionsToIncrease = new ObjectDoubleOpenHashMap<Expression>();
 
-	/**
-	 * Variable for storing maximum difference between expression value and mean.
-	 */
-	private double maxDifferenceIncreaseMetric = Double.MIN_VALUE;
-	private double maxDifferenceDecreaseMetric = Double.MIN_VALUE;
+    /*
+     * Expressions to Decrease along with difference from the mean. key: expression
+     */
+    private ObjectDoubleOpenHashMap<Expression> mMapexpressionsToDecrease = new ObjectDoubleOpenHashMap<Expression>();
 
-	/**
-	 * Constructor to intialize objects of ErrorScoreCalculator, EdgeModifier and
-	 * ConstantValueStorage classes.
-	 * 
-	 * @param errScoreCalculator
-	 * @param edgeModifier
-	 * @param valueCarrier
-	 */
-	public ExpressionChecker(ErrorScoreCalculator_new errScoreCalculator, EdgeModifier edgeModifier,
-			ConstantValueStorage valueCarrier) {
+    /**
+     * Variable to store which metric to increase.
+     */
+    private String metricToIncrease = "";
 
-		mValueCarrier = valueCarrier;
+    /**
+     * Variable to store which metric to decrease.
+     */
+    private String metricToDecrease = "";
 
-		mMapOfMeanValues = errScoreCalculator.getmMapOfMeanValues();
-		mMapPrevMetricsResult = edgeModifier.getmMapPrevMetricsResult();
+    /**
+     * Variable for storing maximum difference between expression value and mean.
+     */
+    private double maxDifferenceIncreaseMetric = Double.MIN_VALUE;
+    private double maxDifferenceDecreaseMetric = Double.MIN_VALUE;
 
-	}
+    /**
+     * Constructor to intialize objects of ErrorScoreCalculator, EdgeModifier and
+     * ConstantValueStorage classes.
+     * 
+     * @param errScoreCalculator
+     * @param edgeModifier
+     * @param valueCarrier
+     */
+    public ExpressionChecker(ErrorScoreCalculator_new errScoreCalculator, EdgeModifier edgeModifier,
+            ConstantValueStorage valueCarrier) {
 
-	/**
-	 * Method to store the expressions along with difference between the expression
-	 * value and mean value.
-	 * 
-	 * @param mapMetricValues
-	 */
-	public void storeExpressions(ObjectDoubleOpenHashMap<String> mapMetricValues) {
-		if (mapMetricValues != null && (mapMetricValues.size()) > 0) {
+        mValueCarrier = valueCarrier;
 
-			Map<Expression, Map<String, Double>> mapConstantValues = mValueCarrier.getMapConstantValues();
-			if (mapConstantValues != null) {
-				Set<Expression> setExpressions = mapConstantValues.keySet();
-				for (Expression expr : setExpressions) {
-					String key = expr.toString();
+        mMapOfMeanValues = errScoreCalculator.getmMapOfMeanValues();
+        mMapPrevMetricsResult = edgeModifier.getmMapPrevMetricsResult();
 
-					double meanValue = mMapOfMeanValues.get(key);
-					double constVal = expr.getValue(mapMetricValues);
-					double difference = constVal - meanValue;
+    }
 
-					System.out.println("Expression : " + key + ", Difference with mean : " + difference);
-					System.out.println("Mean Value : " + meanValue);
-					System.out.println("Expression Value : " + constVal);
-					System.out.println();
+    /**
+     * Constructor to intialize objects of ErrorScoreCalculator, EdgeModifier and
+     * ConstantValueStorage classes.
+     * 
+     * @param errScoreCalculator
+     * @param edgeModifier
+     * @param valueCarrier
+     */
+    public ExpressionChecker(Set<Expression> manualExpressionsSet, ObjectDoubleOpenHashMap<String> mMapOfMeanValues) {
 
-					if (difference > 0.0) {
-						mMapexpressionsToDecrease.put(expr, difference);
-					} else {
-						mMapexpressionsToIncrease.put(expr, Math.abs(difference));
-					}
+        setManualExpressionsSet(manualExpressionsSet);
+        setmMapOfMeanValues(mMapOfMeanValues);
 
-				}
-			} else {
-				LOGGER.warn("The map metric values is invalid");
-			}
+    }
 
-		}
-	}
+    /**
+     * Method to store the expressions along with difference between the expression
+     * value and mean value. This is an updated method which uses manually
+     * configured expression directly.
+     * 
+     * @param mapMetricValues
+     */
+    public void storeExpressions1(ObjectDoubleOpenHashMap<String> mapMetricValues) {
+        if (mapMetricValues != null && (mapMetricValues.size()) > 0) {
 
-	/**
-	 * Method iterates over all the expressions and further calls computeMetrics to
-	 * compute which metrics can increased or decreased.
-	 */
-	public void checkExpressions() {
+            for (Expression expr : manualExpressionsSet) {
+                String key = expr.toString();
 
-		// Logic for Expressions for which values need to be reduced.
-		Object[] keys = mMapexpressionsToDecrease.keys;
-		GetMetricsFromExpressions getMetrics = new GetMetricsFromExpressions();
-		for (Object expr : keys) {
-			if (expr != null) {
-				getMetrics.compute((Expression) expr);
-				Set<String> tempMetrics1 = getMetrics.getDirectProportionalMetricsSet();
-				Set<String> tempMetrics2 = getMetrics.getInverseProportionalMetricsSet();
+                double meanValue = mMapOfMeanValues.get(key);
+                double constVal = expr.getValue(mapMetricValues);
+                double difference = constVal - meanValue;
 
-				computeMetrics((Expression) expr, tempMetrics1, tempMetrics2, mMapexpressionsToDecrease);
-			}
+                System.out.println("Expression : " + key + ", Difference with mean : " + difference);
+                System.out.println("Mean Value : " + meanValue);
+                System.out.println("Expression Value : " + constVal);
+                System.out.println();
 
-		}
+                if (difference > 0.0) {
+                    mMapexpressionsToDecrease.put(expr, difference);
+                } else {
+                    mMapexpressionsToIncrease.put(expr, Math.abs(difference));
+                }
 
-		// Logic for Expressions for which values need to be increased.
-		keys = mMapexpressionsToIncrease.keys;
-		getMetrics = new GetMetricsFromExpressions();
-		for (Object expr : keys) {
-			if (expr != null) {
-				getMetrics.compute((Expression) expr);
-				Set<String> tempMetrics1 = getMetrics.getDirectProportionalMetricsSet();
-				Set<String> tempMetrics2 = getMetrics.getInverseProportionalMetricsSet();
+            }
 
-				computeMetrics((Expression) expr, tempMetrics2, tempMetrics1, mMapexpressionsToIncrease);
-			}
+        }
+    }
 
-		}
+    /**
+     * Method to store the expressions along with difference between the expression
+     * value and mean value.
+     * 
+     * @param mapMetricValues
+     */
+    public void storeExpressions(ObjectDoubleOpenHashMap<String> mapMetricValues) {
+        if (mapMetricValues != null && (mapMetricValues.size()) > 0) {
 
-	}
+            Map<Expression, Map<String, Double>> mapConstantValues = mValueCarrier.getMapConstantValues();
+            if (mapConstantValues != null) {
+                Set<Expression> setExpressions = new HashSet<>();
 
-	/**
-	 * Method to computes the metrics to increase or decrease based on difference
-	 * value.
-	 * 
-	 * @param expr
-	 * @param setMetricsToDecrease
-	 * @param setMetricsToIncrease
-	 * @param mMapexpressions
-	 */
-	private void computeMetrics(Expression expr, Set<String> setMetricsToDecrease, Set<String> setMetricsToIncrease,
-			ObjectDoubleOpenHashMap<Expression> mMapexpressions) {
+                if (!manualExpression) {
+                    setExpressions = mapConstantValues.keySet();
+                } else {
 
-		// Remove Duplicate from both set
-		Set<String> duplicateSet = new HashSet<String>(setMetricsToDecrease);
-		duplicateSet.retainAll(setMetricsToIncrease);
+                }
 
-		setMetricsToDecrease.removeAll(duplicateSet);
-		setMetricsToIncrease.removeAll(duplicateSet);
+                for (Expression expr : setExpressions) {
+                    String key = expr.toString();
 
-		for (String metric : setMetricsToDecrease) {
-			if (maxDifferenceDecreaseMetric < mMapexpressions.get(expr)) {
-				if (!metric.equals("#vertices") && !metric.equals("#edges")) {
-					metricToDecrease = metric;
-					maxDifferenceDecreaseMetric = mMapexpressions.get(expr);
-				}
-			}
-		}
+                    double meanValue = mMapOfMeanValues.get(key);
+                    double constVal = expr.getValue(mapMetricValues);
+                    double difference = constVal - meanValue;
 
-		for (String metric : setMetricsToIncrease) {
-			if (maxDifferenceIncreaseMetric < mMapexpressions.get(expr)) {
-				if (!metric.equals("#vertices") && !metric.equals("#edges")) {
-					metricToIncrease = metric;
-					maxDifferenceIncreaseMetric = mMapexpressions.get(expr);
-				}
-			}
-		}
-	}
+                    System.out.println("Expression : " + key + ", Difference with mean : " + difference);
+                    System.out.println("Mean Value : " + meanValue);
+                    System.out.println("Expression Value : " + constVal);
+                    System.out.println();
 
-	public String getMetricToIncrease() {
-		return metricToIncrease;
-	}
+                    if (difference > 0.0) {
+                        mMapexpressionsToDecrease.put(expr, difference);
+                    } else {
+                        mMapexpressionsToIncrease.put(expr, Math.abs(difference));
+                    }
 
-	public String getMetricToDecrease() {
-		return metricToDecrease;
-	}
+                }
+            } else {
+                LOGGER.warn("The map metric values is invalid");
+            }
 
-	public double getMaxDifferenceIncreaseMetric() {
-		return maxDifferenceIncreaseMetric;
-	}
+        }
+    }
 
-	public double getMaxDifferenceDecreaseMetric() {
-		return maxDifferenceDecreaseMetric;
-	}
+    /**
+     * Method iterates over all the expressions and further calls computeMetrics to
+     * compute which metrics can increased or decreased.
+     */
+    public void checkExpressions() {
+
+        // Logic for Expressions for which values need to be reduced.
+        Object[] keys = mMapexpressionsToDecrease.keys;
+        GetMetricsFromExpressions getMetrics = new GetMetricsFromExpressions();
+        for (Object expr : keys) {
+            if (expr != null) {
+                getMetrics.compute((Expression) expr);
+                Set<String> tempMetrics1 = getMetrics.getDirectProportionalMetricsSet();
+                Set<String> tempMetrics2 = getMetrics.getInverseProportionalMetricsSet();
+
+                computeMetrics((Expression) expr, tempMetrics1, tempMetrics2, mMapexpressionsToDecrease);
+            }
+
+        }
+
+        // Logic for Expressions for which values need to be increased.
+        keys = mMapexpressionsToIncrease.keys;
+        getMetrics = new GetMetricsFromExpressions();
+        for (Object expr : keys) {
+            if (expr != null) {
+                getMetrics.compute((Expression) expr);
+                Set<String> tempMetrics1 = getMetrics.getDirectProportionalMetricsSet();
+                Set<String> tempMetrics2 = getMetrics.getInverseProportionalMetricsSet();
+
+                computeMetrics((Expression) expr, tempMetrics2, tempMetrics1, mMapexpressionsToIncrease);
+            }
+
+        }
+
+    }
+
+    /**
+     * Method to computes the metrics to increase or decrease based on difference
+     * value.
+     * 
+     * @param expr
+     * @param setMetricsToDecrease
+     * @param setMetricsToIncrease
+     * @param mMapexpressions
+     */
+    private void computeMetrics(Expression expr, Set<String> setMetricsToDecrease, Set<String> setMetricsToIncrease,
+            ObjectDoubleOpenHashMap<Expression> mMapexpressions) {
+
+        // Remove Duplicate from both set
+        Set<String> duplicateSet = new HashSet<String>(setMetricsToDecrease);
+        duplicateSet.retainAll(setMetricsToIncrease);
+
+        setMetricsToDecrease.removeAll(duplicateSet);
+        setMetricsToIncrease.removeAll(duplicateSet);
+
+        for (String metric : setMetricsToDecrease) {
+            if (maxDifferenceDecreaseMetric < mMapexpressions.get(expr)) {
+                if (!metric.equals("#vertices") && !metric.equals("#edges")) {
+                    metricToDecrease = metric;
+                    maxDifferenceDecreaseMetric = mMapexpressions.get(expr);
+                }
+            }
+        }
+
+        for (String metric : setMetricsToIncrease) {
+            if (maxDifferenceIncreaseMetric < mMapexpressions.get(expr)) {
+                if (!metric.equals("#vertices") && !metric.equals("#edges")) {
+                    metricToIncrease = metric;
+                    maxDifferenceIncreaseMetric = mMapexpressions.get(expr);
+                }
+            }
+        }
+    }
+
+    public String getMetricToIncrease() {
+        return metricToIncrease;
+    }
+
+    public String getMetricToDecrease() {
+        return metricToDecrease;
+    }
+
+    public double getMaxDifferenceIncreaseMetric() {
+        return maxDifferenceIncreaseMetric;
+    }
+
+    public double getMaxDifferenceDecreaseMetric() {
+        return maxDifferenceDecreaseMetric;
+    }
 
 }
