4933c7258cf7aeaa4b63979dff4708589cc6ecea
pranav.kulkarni <pranavjk@mail.uni-paderborn.de>
2021-11-12 17:12:13 +0100

    Base decorator for ColouredGraph

--- src/main/java/org/aksw/simba/lemming/ColouredGraphDecorator.java
+++ src/main/java/org/aksw/simba/lemming/ColouredGraphDecorator.java
@@ -3,35 +3,36 @@
  */
 package org.aksw.simba.lemming;
 
-import grph.Grph;
+import org.aksw.simba.lemming.mimicgraph.constraints.TripleBaseSingleID;
+
+import it.unimi.dsi.fastutil.ints.IntArrayList;
+import it.unimi.dsi.fastutil.ints.IntSet;
 
 /**
- * Stub wrapper for the ColouredGraph Class
+ * Base Decorator for Coloured Graph Class
  * 
  * @author Pranav
  */
-public class ColouredGraphDecorator {
+public class ColouredGraphDecorator implements IColouredGraph {
 
-    protected ColouredGraph graph;
+    protected IColouredGraph graph;
     protected boolean isAddingEdge;
+    protected TripleBaseSingleID triple;
+
+    public ColouredGraphDecorator() {
+        this.graph = null;
+        this.isAddingEdge = false;
+        this.triple = null;
+    }
 
     /**
      * Class constructor
      * 
      * @param isAddingEdge
      */
-    public ColouredGraphDecorator(ColouredGraph graph, boolean isAddingEdge) {
-        this.graph = graph;
-        this.isAddingEdge = isAddingEdge;
-    }
-
-    /**
-     * Returns the Grph object enclosed within ColouredGraph
-     * 
-     * @return Grph
-     */
-    public Grph getGraph() {
-        return this.graph.getGraph();
+    public ColouredGraphDecorator(ColouredGraph clonedGraph, boolean isAddingEdgeFlag) {
+        this.graph = clonedGraph;
+        this.isAddingEdge = isAddingEdgeFlag;
     }
 
     /**
@@ -39,8 +40,70 @@ public class ColouredGraphDecorator {
      * 
      * @return ColouredGraph graph
      */
-    public ColouredGraph getDecoratedGraph() {
+    public IColouredGraph getGraph() {
         return this.graph;
     }
+
+    public void setTriple(TripleBaseSingleID edge) {
+        this.triple = edge;
+    }
+
+    public TripleBaseSingleID getTriple() {
+        return this.triple;
+    }
+
+    /*
+     * @Override
+     * 
+     * public IntSet getEdgesIncidentTo(int verticeId) { return
+     * this.graph.getEdgesIncidentTo(verticeId); }
+     */
+
+    @Override
+    public int getInEdgeDegree(int vertexId) {
+        return this.graph.getInEdgeDegree(vertexId);
+    }
+
+    @Override
+    public int getOutEdgeDegree(int vertexId) {
+        return this.graph.getOutEdgeDegree(vertexId);
+    }
+
+    @Override
+    public double getMaxInEdgeDegrees() {
+        return this.graph.getMaxInEdgeDegrees();
+    }
+
+    @Override
+    public double getMaxOutEdgeDegrees() {
+        return this.graph.getMaxOutEdgeDegrees();
+    }
+
+    @Override
+    public IntArrayList getAllInEdgeDegrees() {
+        return this.graph.getAllInEdgeDegrees();
+    }
+
+    @Override
+    public IntArrayList getAllOutEdgeDegrees() {
+        return this.graph.getAllOutEdgeDegrees();
+    }
+
+    @Override
+    public double getNumberOfEdges() {
+        return this.graph.getNumberOfEdges();
+    }
+
+    @Override
+    public double getNumberOfVertices() {
+        return this.graph.getNumberOfVertices();
+    }
+
+    /*
+     * @Override
+     * 
+     * public IntSet getVerticesIncidentToEdge(int edgeId) { return
+     * this.graph.getVerticesIncidentToEdge(edgeId); }
+     */
 
 }
