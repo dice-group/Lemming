0fe94eae07a47b5fdab706bea26210f055c68c0e
pranav.kulkarni <pranavjk@mail.uni-paderborn.de>
2021-11-14 06:43:41 +0100

    Modify NumberOfEdgesMetric and NumberOfVerticesMetric

--- src/main/java/org/aksw/simba/lemming/metrics/single/NumberOfEdgesMetric.java
+++ src/main/java/org/aksw/simba/lemming/metrics/single/NumberOfEdgesMetric.java
@@ -1,7 +1,11 @@
 package org.aksw.simba.lemming.metrics.single;
 
 import org.aksw.simba.lemming.ColouredGraph;
+import org.aksw.simba.lemming.ColouredGraphDecorator;
+import org.aksw.simba.lemming.IColouredGraph;
 import org.aksw.simba.lemming.metrics.AbstractMetric;
+import org.aksw.simba.lemming.metrics.single.edgemanipulation.Operation;
+import org.aksw.simba.lemming.mimicgraph.constraints.TripleBaseSingleID;
 
 /**
  * This metric is the number of edges of the graph.
@@ -18,6 +22,19 @@ public class NumberOfEdgesMetric extends AbstractMetric implements SingleValueMe
     @Override
     public double apply(ColouredGraph graph) {
         return graph.getNumberOfEdges();
+    }
+
+    @Override
+    public UpdatableMetricResult applyUpdatable(ColouredGraphDecorator iColouredGraph) {
+        SingleValueMetricResult result = new SingleValueMetricResult(getName(), iColouredGraph.getNumberOfEdges());
+        return result;
+    }
+
+    @Override
+    public UpdatableMetricResult update(ColouredGraphDecorator iColouredGraph, TripleBaseSingleID triple,
+            Operation graphOperation, UpdatableMetricResult previousResult) {
+        ((SingleValueMetricResult) previousResult).setResult(iColouredGraph.getNumberOfEdges());
+        return previousResult;
     }
 
 }
