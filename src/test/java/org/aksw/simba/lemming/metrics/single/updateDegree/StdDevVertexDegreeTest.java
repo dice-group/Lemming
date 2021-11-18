385aa145273b37bf6100816d62339220d347928d
pranav.kulkarni <pranavjk@mail.uni-paderborn.de>
2021-11-18 19:24:18 +0100

    Fix StdDev unit test
    -Add Decorator objects
    -Round only decimal position, not the entire number

--- src/test/java/org/aksw/simba/lemming/metrics/single/updateDegree/StdDevVertexDegreeTest.java
+++ src/test/java/org/aksw/simba/lemming/metrics/single/updateDegree/StdDevVertexDegreeTest.java
@@ -3,7 +3,10 @@ package org.aksw.simba.lemming.metrics.single.updateDegree;
 import grph.Grph;
 import junit.framework.Assert;
 
+import org.aksw.simba.lemming.AddEdgeDecorator;
 import org.aksw.simba.lemming.ColouredGraph;
+import org.aksw.simba.lemming.ColouredGraphDecorator;
+import org.aksw.simba.lemming.RemoveEdgeDecorator;
 import org.aksw.simba.lemming.metrics.single.StdDevVertexDegree;
 import org.aksw.simba.lemming.metrics.single.UpdatableMetricResult;
 import org.aksw.simba.lemming.metrics.single.edgemanipulation.Operation;
@@ -30,25 +33,35 @@ public class StdDevVertexDegreeTest extends UpdateMetricTest {
         // Remove and Add edge between v1 and v2
         addRemoveSameEdge(metric, graph, 0, 1, 1);
         metric = new StdDevVertexDegree(Grph.DIRECTION.out);
-        // Remove and Add edge between v1 and v1
-        addRemoveSameEdge(metric, graph, 0, 0, 0);
+        // Remove and Add edge between v1 and v2
+        addRemoveSameEdge(metric, graph, 0, 1, 2);
     }
 
     void addRemoveSameEdge(StdDevVertexDegree metric, ColouredGraph graph, int tailId, int headId, int edgeId) {
         // check applyUpdatable method
-        UpdatableMetricResult prevResult = metric.applyUpdatable(graph);
-        Assert.assertEquals(Math.round(metric.apply(graph)), Math.round(prevResult.getResult()));
+        ColouredGraphDecorator iColouredGraph = new ColouredGraphDecorator(graph);
+        UpdatableMetricResult prevResult = metric.applyUpdatable(iColouredGraph);
+        Assert.assertEquals(fixTo6DecimalPlaces(metric.apply(graph)), fixTo6DecimalPlaces(prevResult.getResult()));
+
         // delete an edge
+        ColouredGraphDecorator removeDecorator = new RemoveEdgeDecorator(graph, true);
         TripleBaseSingleID triple = new TripleBaseSingleID(tailId, null, headId, null, edgeId, null);
+        removeDecorator.setTriple(triple);
+        prevResult = metric.update(removeDecorator, triple, Operation.REMOVE, prevResult);
         graph = removeEdge(graph, triple.edgeId);
-        prevResult = metric.update(graph, triple, Operation.REMOVE, prevResult);
-        Assert.assertEquals(Math.round(metric.apply(graph)), Math.round(prevResult.getResult()));
+        Assert.assertEquals(fixTo6DecimalPlaces(metric.apply(graph)), fixTo6DecimalPlaces(prevResult.getResult()));
 
         // add same edge
+        ColouredGraphDecorator addDecorator = new AddEdgeDecorator(graph, true);
+        addDecorator.setTriple(triple);
+        prevResult = metric.update(addDecorator, triple, Operation.ADD, prevResult);
         graph = addEdge(graph, tailId, headId);
-        prevResult = metric.update(graph, triple, Operation.ADD, prevResult);
-        Assert.assertEquals(Math.round(metric.apply(graph)), Math.round(prevResult.getResult()));
+        Assert.assertEquals(fixTo6DecimalPlaces(metric.apply(graph)), fixTo6DecimalPlaces(prevResult.getResult()));
 
+    }
+
+    private double fixTo6DecimalPlaces(double number) {
+        return Math.round(number * 100000) / 100000.0d;
     }
 
 }
