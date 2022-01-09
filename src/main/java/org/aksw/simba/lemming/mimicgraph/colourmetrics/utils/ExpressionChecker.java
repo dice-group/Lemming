cdbabe3a86e189a6a876ede8a7785691889289e6
apundir <apundir@campus.uni-paderborn.de>
2022-01-09 20:08:53 +0100

    Removing ErrorScoreCalculator and ValueCarrier objects instead using maps

--- src/main/java/org/aksw/simba/lemming/mimicgraph/colourmetrics/utils/ExpressionChecker.java
+++ src/main/java/org/aksw/simba/lemming/mimicgraph/colourmetrics/utils/ExpressionChecker.java
@@ -5,7 +5,6 @@ import java.util.Map;
 import java.util.Set;
 
 import org.aksw.simba.lemming.algo.expression.Expression;
-import org.aksw.simba.lemming.mimicgraph.metricstorage.ConstantValueStorage;
 import org.apache.commons.lang3.math.NumberUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -23,23 +22,20 @@ public class ExpressionChecker {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(ExpressionChecker.class);
 
+    /**
+     *  Set to store manually defined expressions.
+     */
     private Set<Expression> manualExpressionsSet = new HashSet<>();
-
-    public void setManualExpressionsSet(Set<Expression> manualExpressionsSet) {
-        this.manualExpressionsSet = manualExpressionsSet;
-    }
 
     /*
      * mean value of each expression key: expression
      */
     private ObjectDoubleOpenHashMap<String> mMapOfMeanValues;
-
-    public void setmMapOfMeanValues(ObjectDoubleOpenHashMap<String> mMapOfMeanValues) {
-        this.mMapOfMeanValues = new ObjectDoubleOpenHashMap<>();
-        this.mMapOfMeanValues = mMapOfMeanValues;
-    }
-
-    private ConstantValueStorage mValueCarrier;
+    
+    /**
+     * Map for expressions along with different values for input graphs.
+     */
+    private Map<Expression, Map<String, Double>> mapConstantValues;
 
     /*
      * Expressions to Increase along with difference from the mean. key: expression
@@ -67,25 +63,26 @@ public class ExpressionChecker {
     private double maxDifferenceIncreaseMetric = Double.MIN_VALUE;
     private double maxDifferenceDecreaseMetric = Double.MIN_VALUE;
 
+    
     /**
-     * Constructor to intialize objects of ErrorScoreCalculator, EdgeModifier and
-     * ConstantValueStorage classes.
+     * Constructor to intialize maps of Expression mean values and expression values for input graphs.
      * 
-     * @param errScoreCalculator
-     * @param edgeModifier
-     * @param valueCarrier
+     * @param mMapOfMeanValuesOrg - Map for expressions along with their mean values
+     * @param mapConstantValuesOrg - Map for expressions along with different values for input graphs.
      */
-    public ExpressionChecker(ErrorScoreCalculator errScoreCalculator, ConstantValueStorage valueCarrier) {
+    public ExpressionChecker(ObjectDoubleOpenHashMap<String> mMapOfMeanValuesOrg, Map<Expression, Map<String, Double>> mapConstantValuesOrg) {
 
-        mValueCarrier = valueCarrier;
-
-        mMapOfMeanValues = errScoreCalculator.getmMapOfMeanValues();
+        mapConstantValues = mapConstantValuesOrg;
+        mMapOfMeanValues = mMapOfMeanValuesOrg;
 
     }
 
     /**
      * Constructor to intialize objects of ErrorScoreCalculator, EdgeModifier and
      * ConstantValueStorage classes.
+     * 
+     * Note - This constructor is created for testing purposes. In order to check logic
+     * for manually configured expressions.
      * 
      * @param errScoreCalculator
      * @param edgeModifier
@@ -102,6 +99,9 @@ public class ExpressionChecker {
      * Method to store the expressions along with difference between the expression
      * value and mean value. This is an updated method which uses manually
      * configured expression directly.
+     * 
+     * Note - This method is created for testing purposes. In order to check logic
+     * for manually configured expressions.
      * 
      * @param mapMetricValues
      */
@@ -136,11 +136,12 @@ public class ExpressionChecker {
      * value and mean value.
      * 
      * @param mapMetricValues
+     *            - Map which contains expression as key and expression result as
+     *            value
      */
     public void storeExpressions(ObjectDoubleOpenHashMap<String> mapMetricValues) {
         if (mapMetricValues != null && (mapMetricValues.size()) > 0) {
 
-            Map<Expression, Map<String, Double>> mapConstantValues = mValueCarrier.getMapConstantValues();
             if (mapConstantValues != null) {
                 Set<Expression> setExpressions = new HashSet<>();
                 setExpressions = mapConstantValues.keySet();
@@ -152,10 +153,10 @@ public class ExpressionChecker {
                     double constVal = expr.getValue(mapMetricValues);
                     double difference = constVal - meanValue;
 
-                    System.out.println("Expression : " + key + ", Difference with mean : " + difference);
-                    System.out.println("Mean Value : " + meanValue);
-                    System.out.println("Expression Value : " + constVal);
-                    System.out.println();
+                    //System.out.println("Expression : " + key + ", Difference with mean : " + difference);
+                    //System.out.println("Mean Value : " + meanValue);
+                    //System.out.println("Expression Value : " + constVal);
+                    //System.out.println();
 
                     if (difference > 0.0) {
                         mMapexpressionsToDecrease.put(expr, difference);
@@ -212,9 +213,14 @@ public class ExpressionChecker {
      * value.
      * 
      * @param expr
+     *            - expression
      * @param setMetricsToDecrease
+     *            - set of metrics that can be decreased
      * @param setMetricsToIncrease
+     *            - set of metrics that can be increase
      * @param mMapexpressions
+     *            - Map which contains expression as key and difference of the
+     *            expression with mean as value
      */
     private void computeMetrics(Expression expr, Set<String> setMetricsToDecrease, Set<String> setMetricsToIncrease,
             ObjectDoubleOpenHashMap<Expression> mMapexpressions) {
@@ -245,20 +251,93 @@ public class ExpressionChecker {
         }
     }
 
+    /**
+     * The method returns the metric name that should be increased.
+     * 
+     * @return - metric name
+     */
     public String getMetricToIncrease() {
         return metricToIncrease;
     }
 
+    /**
+     * The method returns the metric name that should be decreased.
+     * 
+     * @return - metric name
+     */
     public String getMetricToDecrease() {
         return metricToDecrease;
     }
 
+    /**
+     * From a given set of expressions, we will identify the metric that should be
+     * increased and the corresponding expression which has the metric. The method
+     * returns the maximum difference of the such an expression value with the mean
+     * value.
+     * 
+     * @return - difference as double value
+     */
     public double getMaxDifferenceIncreaseMetric() {
         return maxDifferenceIncreaseMetric;
     }
 
+    /**
+     * From a given set of expressions, we will identify the metric that should be
+     * decreased and the corresponding expression which has the metric. The method
+     * returns the maximum difference of the such an expression value with the mean
+     * value.
+     * 
+     * @return - difference as double value
+     */
     public double getMaxDifferenceDecreaseMetric() {
         return maxDifferenceDecreaseMetric;
+    }
+    
+    /**
+     * Method to updates map of mean values. This map contains the expression along with
+     * their mean values.
+     * 
+     * @param mMapOfMeanValues
+     *            - Hash Map with expression key and mean as value.
+     */
+    public void setmMapOfMeanValues(ObjectDoubleOpenHashMap<String> mMapOfMeanValues) {
+        this.mMapOfMeanValues = mMapOfMeanValues;
+    }
+    
+    /**
+     * The method to updates manually defined expressions.
+     * 
+     * Note - This method is created for testing purposes. In order to check logic
+     * for manually configured expressions.
+     * 
+     * @param manualExpressionsSet
+     *            - set of expressions
+     */
+    public void setManualExpressionsSet(Set<Expression> manualExpressionsSet) {
+        this.manualExpressionsSet = manualExpressionsSet;
+    }
+
+    /**
+     * From a given set of expressions, we will identify the metric that should be
+     * increased and the corresponding expression which has the metric. The method
+     * updates the maximum difference of the such an expression value with the mean
+     * value.
+     * @param maxDifferenceIncreaseMetric
+     */
+    public void setMaxDifferenceIncreaseMetric(double maxDifferenceIncreaseMetric) {
+        this.maxDifferenceIncreaseMetric = maxDifferenceIncreaseMetric;
+    }
+
+
+    /**
+     * From a given set of expressions, we will identify the metric that should be
+     * decreased and the corresponding expression which has the metric. The method
+     * returns the maximum difference of the such an expression value with the mean
+     * value.
+     * @param maxDifferenceDecreaseMetric - double value
+     */
+    public void setMaxDifferenceDecreaseMetric(double maxDifferenceDecreaseMetric) {
+        this.maxDifferenceDecreaseMetric = maxDifferenceDecreaseMetric;
     }
 
 }
