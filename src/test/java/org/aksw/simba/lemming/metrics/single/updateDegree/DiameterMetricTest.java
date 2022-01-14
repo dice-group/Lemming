38eb4c9bcc6ea436f7d154197c90f9632779686d
pranav.kulkarni <pranavjk@mail.uni-paderborn.de>
2022-01-14 09:36:02 +0100

    simulate graph optimization phase in DiameterMetricTest

--- src/test/java/org/aksw/simba/lemming/metrics/single/updateDegree/DiameterMetricTest.java
+++ src/test/java/org/aksw/simba/lemming/metrics/single/updateDegree/DiameterMetricTest.java
@@ -6,7 +6,9 @@ package org.aksw.simba.lemming.metrics.single.updateDegree;
 import org.aksw.simba.lemming.AddEdgeDecorator;
 import org.aksw.simba.lemming.ColouredGraph;
 import org.aksw.simba.lemming.ColouredGraphDecorator;
+import org.aksw.simba.lemming.RemoveEdgeDecorator;
 import org.aksw.simba.lemming.metrics.single.DiameterMetric;
+import org.aksw.simba.lemming.metrics.single.DiameterMetricResult;
 import org.aksw.simba.lemming.metrics.single.UpdatableMetricResult;
 import org.aksw.simba.lemming.metrics.single.edgemanipulation.Operation;
 import org.aksw.simba.lemming.mimicgraph.constraints.TripleBaseSingleID;
@@ -85,8 +87,10 @@ public class DiameterMetricTest {
         int[] nodeList = new int[numberOfNodes];
         for (int i = 0; i < numberOfNodes; i++) {
             nodeList[i] = graph.addVertex();
-
         }
+        // Add Random Edges
+        graph.addDirectedSimpleEdge(numberOfNodes / numberOfNodes, 2 * numberOfNodes % (numberOfNodes - 1));
+        graph.addDirectedSimpleEdge(numberOfNodes / numberOfNodes, 3 * numberOfNodes % (numberOfNodes - 1));
         return new ColouredGraph(graph, null, null);
     }
 
@@ -96,16 +100,6 @@ public class DiameterMetricTest {
         ColouredGraph graph = buildLinearGraph();
         UpdatableMetricResult result = metric.applyUpdatable(new ColouredGraphDecorator(graph));
         Assert.assertEquals(numberOfNodes - 1, (int) result.getResult());
-
-        // Add an Edge from middle node to the end node
-        int headId = 5;
-        int tailId = 0;
-        TripleBaseSingleID triple = new TripleBaseSingleID(tailId, null, headId, null, numberOfNodes - 1, null);
-        ColouredGraphDecorator graphDecorator = new AddEdgeDecorator(graph, true);
-        graphDecorator.setTriple(triple);
-        result = metric.update(graphDecorator, triple, Operation.ADD, result);
-        graph.addEdge(tailId, headId);
-        Assert.assertEquals(numberOfNodes - headId, (int) result.getResult());
     }
 
     @Test
@@ -127,6 +121,67 @@ public class DiameterMetricTest {
         DiameterMetric metric = new DiameterMetric();
         ColouredGraph graph = buildDisconnectedGraph();
         Assert.assertEquals(0, (int) metric.apply(graph));
+    }
+
+    @Test
+    public void simulateGraphOptimizationPhase() {
+        // Build graph
+        Grph graph = new InMemoryGrph();
+        int numberOfNodes = 5;
+        int[] nodeList = new int[numberOfNodes];
+        for (int i = 0; i < numberOfNodes; i++) {
+            nodeList[i] = graph.addVertex();
+        }
+        graph.addDirectedSimpleEdge(0, 1);
+        graph.addDirectedSimpleEdge(0, 2);
+        graph.addDirectedSimpleEdge(1, 2);
+        graph.addDirectedSimpleEdge(2, 3);
+        graph.addDirectedSimpleEdge(2, 4);
+        graph.addDirectedSimpleEdge(3, 4);
+        // Nodes: 0,1,2,3,4
+        // Diameters: 0-2-3, 0-2-4, 
+
+        DiameterMetric metric = new DiameterMetric();
+
+        ColouredGraphDecorator graphDec = new ColouredGraphDecorator(new ColouredGraph(graph, null, null));
+        AddEdgeDecorator addDec = new AddEdgeDecorator(new ColouredGraph(graph, null, null), true);
+        RemoveEdgeDecorator remDec = new RemoveEdgeDecorator(new ColouredGraph(graph, null, null), false);
+
+        DiameterMetricResult resultObj = (DiameterMetricResult) metric.applyUpdatable(graphDec);
+        Assert.assertEquals(2.0, resultObj.getResult());
+        Assert.assertEquals(2, resultObj.getCountOfDiameters());
+        
+        // Addition of an edge that does not affect the diameter
+        TripleBaseSingleID triple = new TripleBaseSingleID(2, null, 3, null, numberOfNodes + 1, null);
+        addDec.setTriple(triple);
+        resultObj = (DiameterMetricResult) metric.update(addDec, triple, Operation.ADD, resultObj);
+        Assert.assertEquals(2.0, resultObj.getResult());
+        Assert.assertEquals(2, resultObj.getCountOfDiameters());
+        
+        // Addition of an edge that shortens the diameter
+        triple = new TripleBaseSingleID(0, null, 4, null, numberOfNodes + 2, null);
+        addDec.setTriple(triple);
+        resultObj = (DiameterMetricResult) metric.update(addDec, triple, Operation.ADD, resultObj);
+        //Result is still 2 because a Path exists in nodes 0-2-3
+        Assert.assertEquals(2.0, resultObj.getResult());
+        Assert.assertEquals(1, resultObj.getCountOfDiameters());
+        
+        // Reset the result object back to the initial state 
+        resultObj = (DiameterMetricResult) metric.applyUpdatable(graphDec);
+        
+        // Removal of an edge that does not affect the diameter
+        triple = new TripleBaseSingleID(1, null, 2, null, 2, null);
+        remDec.setTriple(triple);
+        resultObj = (DiameterMetricResult) metric.update(remDec, triple, Operation.REMOVE, resultObj);
+        Assert.assertEquals(2.0, resultObj.getResult());
+        Assert.assertEquals(2, resultObj.getCountOfDiameters());
+        
+        // Removal of an edge that changes the diameter
+        triple = new TripleBaseSingleID(0, null, 2, null, 1, null);
+        remDec.setTriple(triple);
+        resultObj = (DiameterMetricResult) metric.update(remDec, triple, Operation.REMOVE, resultObj);
+        Assert.assertEquals(3.0, resultObj.getResult());
+        Assert.assertEquals(1, resultObj.getCountOfDiameters());
     }
 
 }
