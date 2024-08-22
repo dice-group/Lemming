8fb9f6d69f349768a390ebf65bab03403043a359
aalexandrams@gmail.com <aalexandrams@gmail.com>
2024-08-22 16:07:31 +0200

    Added sub-rings for graphs with k<2

--- src/main/java/org/aksw/simba/lemming/mimicgraph/generator/baseline/DirectedWattsStrogatz.java
+++ src/main/java/org/aksw/simba/lemming/mimicgraph/generator/baseline/DirectedWattsStrogatz.java
@@ -12,8 +12,10 @@ import grph.Grph;
 import grph.algo.topology.RandomNewmanWattsStrogatzTopologyGenerator;
 import grph.in_memory.InMemoryGrph;
 import it.unimi.dsi.fastutil.ints.IntArraySet;
+import it.unimi.dsi.fastutil.ints.IntIterator;
 import it.unimi.dsi.fastutil.ints.IntSet;
 import toools.collections.primitive.IntCursor;
+import toools.collections.primitive.SelfAdaptiveIntSet;
 
 /**
  * 
@@ -39,9 +41,14 @@ public class DirectedWattsStrogatz implements IGenerator {
 
 		// get undirected graph
 		int n = g.getVertices().size();
-		g.ring();
-		connectToKClosestNeighbors(g, k);
 
+		if (k >= 2) {
+			g.ring();
+			connectToKClosestNeighbors(g, k);
+		} else {
+			partialRing(g, (int) Math.floor(k));
+		}
+		
 		// if the network is a clique
 		if (k < n / 2) {
 			for (int u : g.getVertices().toIntArray()) {
@@ -80,10 +87,33 @@ public class DirectedWattsStrogatz implements IGenerator {
 		}
 	}
 
+	public void partialRing(Grph graph, int k) {
+		 // Create an empty set to hold the edges
+		IntSet vertices = graph.getVertices();
+	    if (vertices.size() > 1) {
+	        int numEdges = vertices.size() * k / 2; // Total edges to meet the average degree
+	        IntIterator i = vertices.iterator();
+	        int predecessor = i.nextInt();
+	        int first = predecessor;
+
+	        // Add the necessary edges
+	        for (int j = 0; j < numEdges && i.hasNext(); j++) {
+	            int a = i.nextInt();
+	            graph.addSimpleEdge(predecessor, a, false);
+	            predecessor = a;
+	        }
+
+	        // Close the loop if there's still an edge left to add
+	        if (numEdges > 0 && numEdges % vertices.size() != 0) {
+	        	graph.addSimpleEdge(predecessor, first, false);
+	        }
+	    } 
+	}
+
 	private void connectToKClosestNeighbors(Grph graph, double k) {
 
 		int noVertices = graph.getVertices().getGreatest() + 1;
-		
+
 		// assign expected contributions of each vertex
 		int[] edgeContributions = assignEdges(noVertices, k);
 
@@ -98,16 +128,16 @@ public class DirectedWattsStrogatz implements IGenerator {
 			// right side neighbours first
 			for (IntCursor n : IntCursor.fromFastUtil(rightNe)) {
 				// check if connection already exists before adding it
-				if (expectedContribution>0 && graph.getEdgesConnecting(v.value, n.value).isEmpty()) {
+				if (expectedContribution > 0 && graph.getEdgesConnecting(v.value, n.value).isEmpty()) {
 					expectedContribution--;
 					graph.addUndirectedSimpleEdge(v.value, n.value);
 				}
 			}
-			
+
 			// left side now
 			for (IntCursor n : IntCursor.fromFastUtil(leftNe)) {
 				// check if connection already exists before adding it
-				if (expectedContribution>0 && graph.getEdgesConnecting(v.value, n.value).isEmpty()) {
+				if (expectedContribution > 0 && graph.getEdgesConnecting(v.value, n.value).isEmpty()) {
 					expectedContribution--;
 					graph.addUndirectedSimpleEdge(v.value, n.value);
 				}
@@ -161,10 +191,10 @@ public class DirectedWattsStrogatz implements IGenerator {
 		}
 
 		// create extra edges with leftover randomly
-		int leftoverEdges = (int) Math.floor((k - roundK) * vertices) ;
+		int leftoverEdges = (int) Math.floor((k - roundK) * vertices);
 		for (int i = 0; i < leftoverEdges; i++) {
 			int vertex = rnd.nextInt(vertices);
-			contributions[vertex] ++;
+			contributions[vertex]++;
 		}
 
 		return contributions;
@@ -229,7 +259,7 @@ public class DirectedWattsStrogatz implements IGenerator {
 		return range;
 	}
 
-	public void setK(int k) {
+	public void setK(double k) {
 		if (k < 0)
 			throw new IllegalArgumentException("k must be >= 0");
 
@@ -265,7 +295,7 @@ public class DirectedWattsStrogatz implements IGenerator {
 	 * @param k Number of neighbours
 	 * @param p Probability of rewiring
 	 */
-	public static void compute(Grph g, int k, double p, long seed) {
+	public static void compute(Grph g, double k, double p, long seed) {
 		DirectedWattsStrogatz tg = new DirectedWattsStrogatz();
 		tg.setPRNG(new Random(seed));
 		tg.setK(k);
@@ -280,11 +310,11 @@ public class DirectedWattsStrogatz implements IGenerator {
 		DirectedWattsStrogatz.compute(g, (int) Math.floor(avgDegree), 0.5, seed);
 		return g;
 	}
-	
+
 	public static void main(String[] args) {
 		Grph g = new InMemoryGrph();
 		g.addNVertices(7);
-		DirectedWattsStrogatz.compute(g, 4, 0.5, 123);
+		DirectedWattsStrogatz.compute(g, 1.2, 0.5, 123);
 	}
 
 }
