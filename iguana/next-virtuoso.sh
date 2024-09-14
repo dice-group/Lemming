3a5589547ce9ed272617688fd64aa1dc591c7de6
aalexandrams@gmail.com <aalexandrams@gmail.com>
2024-09-14 13:33:58 +0200

    Fix script

--- iguana/next-virtuoso.sh
+++ iguana/next-virtuoso.sh
@@ -16,7 +16,7 @@ do
     echo "$gn"
     cp example-suite.yml example-suiteTMP.yml
     queries=$( ./get_queries.sh $f )
-    save="${file_name}-Fuseki"
+    save="${file_name}-Virtuoso"
     echo "Queries file: $queries"
     sed -i -e 's,QUERYFILE,'"$queries"',g' example-suiteTMP.yml
     sed -i -e 's,GraphName,'"$gn"',g' example-suiteTMP.yml
