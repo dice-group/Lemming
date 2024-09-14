d619826e18bb424f9c1fafb3910260183e565b50
aalexandrams@gmail.com <aalexandrams@gmail.com>
2024-09-14 12:13:41 +0200

    Fix fuseki ref

--- iguana/start-ref.sh
+++ iguana/start-ref.sh
@@ -1,7 +1,7 @@
 pkill -f fuseki
 
-rm -rf /local/Lemming/iguana/fuseki/apache-jena-fuseki-5.1.0/REF/
+rm -rf /local/Lemming/iguana/ref/apache-jena-fuseki-5.1.0/REF/
 
-/local/Lemming/iguana/fuseki/apache-jena-5.1.0/bin/tdb1.xloader --loc /local/Lemming/iguana/fuseki/apache-jena-fuseki-5.1.0/REF $1 
-cd /local/Lemming/iguana/fuseki/apache-jena-fuseki-5.1.0/ && ./fuseki-server --port 3131 --loc=REF /ref &
+/local/Lemming/iguana/ref/apache-jena-5.1.0/bin/tdb1.xloader --loc /local/Lemming/iguana/ref/apache-jena-fuseki-5.1.0/REF $1 
+cd /local/Lemming/iguana/ref/apache-jena-fuseki-5.1.0/ && ./fuseki-server --port 3131 --loc=REF /ref &
 
