38db86bc09f590a39402d77dd8acb5ac7c757d63
Ana Silva <46345715+aalexandrasilva@users.noreply.github.com>
2025-10-07 11:29:50 +0200

    Update run_dataset.sh

--- run_dataset.sh
+++ run_dataset.sh
@@ -4,21 +4,16 @@ dataset=$1
 thrs=$2
 mode='graph'
 opt_iterations=100000
-if [ $dataset = 'pg' ]
+
+if [ $dataset = 'swdf' ]
 then 
-	nv=792923
-elif [ $dataset = 'swdf' ]
-then 
-	nv=45398
+	nv=45221
 elif [ $dataset = 'lgeo' ]
 then 
-	nv=591649
+	nv=591237
 elif [ $dataset = 'geology' ]
 then 
-	nv=1423
-elif [ $dataset = 'dbp' ]
-then 
-	nv=7942015
+	nv=1243
 else
 	echo "Invalid dataset"
 	exit
@@ -27,18 +22,11 @@ fi
 mkdir -p initial
 count=3
 for i in $(seq $count); do
-  java -jar lemming.jar $mode -ds $dataset -nv $nv -thrs $thrs -c UCS -v UIS -op $opt_iterations
-  java -jar lemming.jar $mode -ds $dataset -nv $nv -thrs $thrs -c UCS -v BIS -op $opt_iterations
-  java -jar lemming.jar $mode -ds $dataset -nv $nv -thrs $thrs -c BCS -v UIS -op $opt_iterations
-  java -jar lemming.jar $mode -ds $dataset -nv $nv -thrs $thrs -c BCS -v BIS -op $opt_iterations
-  java -jar lemming.jar $mode -ds $dataset -nv $nv -thrs $thrs -c CCS -v UIS -op $opt_iterations
-  java -jar lemming.jar $mode -ds $dataset -nv $nv -thrs $thrs -c CCS -v BIS -op $opt_iterations
   java -jar lemming.jar $mode -ds $dataset -nv $nv -thrs $thrs -m Simplex -sp BPSI -sc BCSI -op $opt_iterations
   java -jar lemming.jar $mode -ds $dataset -nv $nv -thrs $thrs -m Simplex -sp BPSI -sc UCSI -op $opt_iterations 
   java -jar lemming.jar $mode -ds $dataset -nv $nv -thrs $thrs -m Simplex -sp UPSI -sc BCSI -op $opt_iterations
   java -jar lemming.jar $mode -ds $dataset -nv $nv -thrs $thrs -m Simplex -sp UPSI -sc UCSI -op $opt_iterations
   java -jar lemming.jar $mode -ds $dataset -nv $nv -thrs $thrs -m Bl -bl BA
   java -jar lemming.jar $mode -ds $dataset -nv $nv -thrs $thrs -m Bl -bl WS
-
 done
 
